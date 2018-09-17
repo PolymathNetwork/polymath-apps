49d363a3ddb6351b6a5fa19906cc314a7f52e036
Rafael Vidaurre <narzerus@gmail.com>
2018-09-17 16:25:15 -0300

    build: install flow types
    install flowtype libdefs for all packages

---
+++ packages/polymath-offchain/flow-typed/npm/@sendgrid/mail_v6.x.x.js
@@ -0,0 +1,141 @@
+// flow-typed signature: 8f712e25d78848b810ac44a12abf1e50
+// flow-typed version: d59b485e00/@sendgrid/mail_v6.x.x/flow_>=v0.30.x
+
+declare module '@sendgrid/mail' {
+
+  declare export class ResponseError extends Error {
+    code: number;
+    message: string;
+    response: {
+      headers: { [key: string]: string; };
+      body: string;
+    };
+  }
+
+  declare interface PersonalizationData {
+    to: EmailData | EmailData[],
+    cc?: EmailData | EmailData[],
+    bcc?: EmailData | EmailData[],
+    subject?: string;
+    headers?: { [key: string]: string };
+    substitutions?: { [key: string]: string };
+    customArgs?: { [key: string]: string };
+    sendAt?: number;
+  }
+
+  declare interface AttachmentData {
+    content: string;
+    filename: string;
+    type?: string;
+    disposition?: string;
+    contentId?: string;
+  }
+
+  declare interface ASMOptions {
+    groupId: number;
+    groupsToDisplay?: number[];
+  }
+
+  declare interface MailSettings {
+    bcc?: {
+      enable?: boolean;
+      email?: string;
+    };
+    bypassListManagement?: {
+      enable?: boolean;
+    };
+    footer?: {
+      enable?: boolean;
+      text?: string;
+      html?: string;
+    };
+    sandboxMode?: {
+      enable?: boolean;
+    };
+    spamCheck?: {
+      enable?: boolean;
+      threshold?: number;
+      postToUrl?: string;
+    };
+  }
+
+  declare interface TrackingSettings {
+    clickTracking?: {
+      enable?: boolean;
+      enableText?: boolean;
+    };
+    openTracking?: {
+      enable?: boolean;
+      substitutionTag?: string;
+    };
+    subscriptionTracking?: {
+      enable?: boolean;
+      text?: string;
+      html?: string;
+      substitutionTag?: string;
+    };
+    ganalytics?: {
+      enable?: boolean;
+      utmSource?: string;
+      utmMedium?: string;
+      utmTerm?: string;
+      utmContent?: string;
+      utmCampaign?: string;
+    };
+  }
+
+  declare export type EmailData = string | { name?: string; email: string; };
+
+  declare interface MailContent {
+    type: string;
+    value: string;
+  }
+
+  declare export type MailData = {
+    to?: EmailData | EmailData[],
+    cc?: EmailData | EmailData[],
+    bcc?: EmailData | EmailData[],
+
+    from: EmailData,
+    replyTo?: EmailData,
+
+    sendAt?: number,
+
+    subject?: string,
+    text?: string,
+    html?: string,
+    content?: MailContent[],
+    templateId?: string,
+
+    personalizations?: PersonalizationData[],
+    attachments?: AttachmentData[],
+
+    ipPoolName?: string,
+    batchId?: string,
+
+    sections?: { [key: string]: string },
+    headers?: { [key: string]: string },
+
+    categories?: string[],
+    category?: string,
+
+    customArgs?: { [key: string]: any },
+    asm?: ASMOptions,
+
+    mailSettings?: MailSettings,
+    trackingSettings?: TrackingSettings,
+
+    substitutions?: { [key: string]: string },
+    substitutionWrappers?: string[],
+
+    isMultiple?: boolean,
+  }
+
+  declare class MailService {
+    setApiKey(apiKey: string): void;
+    setSubstitutionWrappers(left: string, right: string): void;
+    send(data: MailData | MailData[], isMultiple?: boolean, cb?: (err: Error | ResponseError, result: [http$ServerResponse, {}]) => void): Promise<[http$ServerResponse, {}]>;
+  }
+
+  declare export default MailService & { MailService: MailService };
+}
