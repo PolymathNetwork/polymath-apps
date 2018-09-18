3442df5e90ad785e580659be7952a9ad7a930121
Rafael Vidaurre <narzerus@gmail.com>
2018-09-18 14:40:47 -0300

    build: add build commands to all repositories

--- packages/polymath-js/webpack.prod.js
+++ packages/polymath-js/webpack.prod.js
@@ -1,4 +1,9 @@
 const merge = require('webpack-merge');
 const base = require('../../config/webpack.prod');
 
-module.exports = merge(base, {});
+const path = require('path');
+module.exports = merge(base, {
+  output: {
+    path: path.join(__dirname, 'build'),
+  },
+});
--- packages/polymath-offchain/webpack.prod.js
+++ packages/polymath-offchain/webpack.prod.js
@@ -1,7 +1,11 @@
 const merge = require('webpack-merge');
+const path = require('path');
 const base = require('../../config/webpack.prod');
 
 module.exports = merge(base, {
+  output: {
+    path: path.join(__dirname, 'build'),
+  },
   node: {
     console: true,
     fs: 'empty',
