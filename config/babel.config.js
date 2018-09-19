6df70a1b4838605ad987c0bf4744f2c2b71cb809
Rafael Vidaurre <narzerus@gmail.com>
2018-09-19 17:28:18 -0300

    build: add module resolution exception for build directories
    An exception for the weird cases where we need to import from a build package. Only applied to local dependencies and specifically to folders named build living in the root of a package

--- config/babel.config.js
+++ config/babel.config.js
@@ -4,11 +4,17 @@ module.exports = {
     {
       root: ['./src'],
       alias: {
+        /* Requirements */
+        '^@polymathnetwork/([^/]+)/build/(.+)':
+          '@polymathnetwork/\\1/build/\\2',
+        /* Resolve local dependencies to their src directory */
         '^@polymathnetwork/([^/]+)/(.+)': '@polymathnetwork/\\1/src/\\2',
         '^@polymathnetwork/(.+)$': '@polymathnetwork/\\1/src',
-        '^@polymathnetwork/(.+)': '@polymathnetwork/\\1',
+        /* Consider package.json as special case */
         '^@polymathnetwork/([^/]+)/package.json':
           '@polymathnetwork/\\1/package.json',
+        /* TODO @RafaelVidaurre: See if this is necessary */
+        '^@polymathnetwork/(.+)': '@polymathnetwork/\\1',
       },
     },
   ],
