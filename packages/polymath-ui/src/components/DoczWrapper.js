92789c3ddc4fba90c6e0165199ad4ee6df01b4eb
Adrien Denat <oi@adriendenat.com>
2018-09-27 19:18:08 -0300

    chore(ui): add Redux store provider as some components are connected

--- packages/polymath-ui/src/components/DoczWrapper.js
+++ packages/polymath-ui/src/components/DoczWrapper.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
+import { uiReducer, networkReducer } from '../redux/reducer';
 
 import '../style.scss';
 
-const Wrapper = ({ children }) => <Provider store={{}}>{children}</Provider>;
+const reducers = combineReducers({ ui: uiReducer, network: networkReducer });
+const store = createStore(reducers);
+
+const Wrapper = ({ children }) => <Provider store={store}>{children}</Provider>;
 
 export default Wrapper;
