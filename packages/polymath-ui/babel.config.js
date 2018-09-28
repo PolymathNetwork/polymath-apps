747754247b71593dc81912306e286fe533704da9
Adrien Denat <oi@adriendenat.com>
2018-09-28 18:23:49 -0300

    chore(ui): refactor babel/webpack/jest config
    make it similar structure than other packages

--- packages/polymath-ui/babel.config.js
+++ packages/polymath-ui/babel.config.js
@@ -1,14 +1,10 @@
 const base = require('../../config/babel.config.js');
+
 module.exports = {
-  babelrcRoots: ['../packages/*'],
-  presets: ['@babel/flow', '@babel/env', '@babel/react'],
+  presets: ['react-app'],
   plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/proposal-export-default-from',
     '@babel/proposal-class-properties',
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-syntax-async-generators',
-    '@babel/plugin-transform-regenerator',
     base.moduleResolver,
   ],
 };
