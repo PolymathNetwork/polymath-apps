0e745c384ee0a08c95ac25e67e4ca13cd5531215
RafaelVidaurre <narzerus@gmail.com>
2018-09-29 17:54:12 -0300

    build: adds test coverage instrumentation for end-to-end testing
    enable by setting the COVERAGE env var to true. Also improves
    overall config files struture
    
    CLOSES ISSUE: #48

--- packages/polymath-investor/babel.config.js
+++ packages/polymath-investor/babel.config.js
@@ -3,9 +3,5 @@ const base = require('../../config/babel.config.js');
 module.exports = {
   babelrcRoots: ['../packages/*'],
   presets: ['react-app'],
-  plugins: [
-    '@babel/proposal-export-default-from',
-    '@babel/proposal-class-properties',
-    base.moduleResolver,
-  ],
+  plugins: [...base.plugins],
 };
