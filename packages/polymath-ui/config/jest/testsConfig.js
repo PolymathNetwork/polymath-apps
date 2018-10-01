779b56df40190a5bfae4108a42734e7f7dfe4be5
Adrien Denat <oi@adriendenat.com>
2018-10-01 13:23:38 -0300

    chore(ui): setup Redux initial state for tests

--- packages/polymath-ui/config/jest/testsConfig.js
+++ packages/polymath-ui/config/jest/testsConfig.js
@@ -1,6 +1,15 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 
-// global.store = store;
+import { uiReducer, networkReducer } from '../../src/redux/reducer';
+
+const mockStore = configureMockStore([thunk]);
+
+global.store = mockStore({
+  ui: uiReducer(undefined, {}),
+  network: networkReducer(undefined, {}),
+});
 
 configure({ adapter: new Adapter() });
