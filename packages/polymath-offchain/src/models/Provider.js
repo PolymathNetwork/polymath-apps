ebcee189d1c98a27be67c0d32cb2256d02f2a700
Jeremias Diaz <monitz87@gmail.com>
2018-10-01 03:18:42 -0300

    docs(offchain): add TODO to Provider model
    The provider model has a superfluous numerical id field. That could be perfectly represented by mongoDB's default _id field

--- packages/polymath-offchain/src/models/Provider.js
+++ packages/polymath-offchain/src/models/Provider.js
@@ -2,6 +2,9 @@
 
 import mongoose from 'mongoose';
 
+// TODO @monitz87: rename id field to avoid clashes with Mongoose$Document
+// and change all references to it (wiki, offchain code, the hardcoded providers and the providers in the offchain database)
+
 export class ProviderDocument /* :: extends Mongoose$Document */ {
   id: number | string;
   email: string;
