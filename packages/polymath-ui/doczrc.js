091f6483632e864a0f184fe3e6bedbb386423787
Adrien Denat <oi@adriendenat.com>
2018-10-01 17:05:47 -0300

    fix(ui): styles were not being applied in Docz

--- packages/polymath-ui/doczrc.js
+++ packages/polymath-ui/doczrc.js
@@ -4,8 +4,7 @@ export default {
   title: 'PolymathUI',
   description: 'This is my awesome documentation',
   files: '**/*.{md,mdx}',
-  // TODO @grsmto: we can't include this wrapper before it imports some polymath-js dependencies that break Docz build. Need to investigate why this is happening.
-  // wrapper: 'src/components/DoczWrapper',
+  wrapper: 'src/components/DoczWrapper',
   plugins: [
     css({
       preprocessor: 'sass',
