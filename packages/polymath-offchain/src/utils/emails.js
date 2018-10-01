e1e6046e62a811a1179e0b3385d4ca7b518b0f85
Jeremias Diaz <monitz87@gmail.com>
2018-10-01 03:39:48 -0300

    refactor(offchain): remove redundant parameter in provider email func
    The parameter is already passed inside the application object. Also improved comments

--- packages/polymath-offchain/src/utils/emails.js
+++ packages/polymath-offchain/src/utils/emails.js
@@ -16,6 +16,15 @@ import { SENDGRID_API_KEY } from '../constants';
 
 import type { EmailData } from '@sendgrid/mail';
 
+/**
+  Sends an email using sendgrid
+
+  @param {string} email email address to send the email to
+  @param {string} name name of the email receiver
+  @param {string} subject email subject
+  @param {string} body email body
+  @param {string | Object} replyTo reply address or object with a reply address and a name
+ */
 const sendEmail = async (
   email: string,
   name: string,
@@ -52,10 +61,29 @@ export type Application = {|
   otherDetails: ?string,
 |};
 
+/**
+  Sends an email to a provider notifying that an issuer has filled out an application form
+
+  The application form has the following data:
+
+  - companyName: the issuer's company name
+  - companyDesc: description of the issuer's company
+  - operatedIn: company's jurisdiction of operation
+  - incorporatedIn: company's jursidiction of incorporation
+  - projectURL: corporate/project presentation URL
+  - profilesURL: management and board member profiles URL
+  - structureURL: corporate or project structure/organization URL
+  - otherDetails: other details about the company
+
+  @param {string} providerEmail email address of the provider
+  @param {string} providerName company name of the provider
+  @param {string} issuerName name of the issuer
+  @param {string} issuerEmail email address of the issuer
+  @param {Object} application application form fields
+ */
 export const sendProviderApplicationEmail = async (
   providerEmail: string,
   providerName: string,
-  companyName: string,
   issuerName: string,
   issuerEmail: string,
   application: Application
@@ -63,7 +91,7 @@ export const sendProviderApplicationEmail = async (
   await sendEmail(
     providerEmail,
     providerName,
-    `${companyName} is interested in your services`,
+    `${application.companyName} is interested in your services`,
     ReactDOMServer.renderToStaticMarkup(
       <ProviderApplication
         issuerName={issuerName}
@@ -78,6 +106,14 @@ export const sendProviderApplicationEmail = async (
   );
 };
 
+/**
+  Sends an email to the issuer notifying him of a ticker being reserved
+
+  @param {string} issuerEmail email address of the issuer
+  @param {string} issuerName name of the issuer
+  @param {string} txHash transaction hash
+  @param {string} expiryLimit number of days before the ticker reservation expires
+ */
 export const sendTickerReservedEmail = async (
   issuerEmail: string,
   issuerName: string,
@@ -99,6 +135,14 @@ export const sendTickerReservedEmail = async (
   );
 };
 
+/**
+  Sends an email to the issuer notifying him that his token has been created
+
+  @param {string} issuerEmail email address of the issuer
+  @param {string} issuerName name of the issuer
+  @param {string} txHash transaction hash
+  @param {string} ticker name of the token
+ */
 export const sendTokenCreatedEmail = async (
   issuerEmail: string,
   issuerName: string,
@@ -115,6 +159,19 @@ export const sendTokenCreatedEmail = async (
   );
 };
 
+/**
+  Sends an email to the issuer notifying him that an STO for his token has been scheduled
+
+  @param {string} issuerEmail email address of the issuer
+  @param {string} issuerName name of the issuer
+  @param {string} txHash transaction hash
+  @param {string} ticker name of the issuer's security token
+  @param {number} cap maximum number of tokens that can be sold
+  @param {string} fundsReceiver wallet address that investment funds will be transferred to
+  @param {boolean} isPolyFundraise true if the funds are raised in POLY, false if they are raised in ETH
+  @param {number} rate conversion rate between currency of choice and the issuer's security token
+  @param {Date} start start date of the STO
+ */
 export const sendSTOScheduledEmail = async (
   issuerEmail: string,
   issuerName: string,
@@ -144,6 +201,13 @@ export const sendSTOScheduledEmail = async (
   );
 };
 
+/**
+  Sends an email to the provided address in order to verify that it belongs to the issuer
+
+  @param {string} issuerEmail address to send the confirmation email to
+  @param {string} issuerName name of the issuer
+  @param {string} pin randomly generated code to verify the email address in the dApp
+ */
 export const sendAccountConfirmationEmail = async (
   issuerEmail: string,
   issuerName: string,
