488d4e7f321379fdadfe37c18d7e02760ed3fee3
Adrien Denat <oi@adriendenat.com>
2018-10-04 15:24:25 -0300

    build(ui): add missing packages to fix Babel build

--- packages/polymath-ui/babel.config.js
+++ packages/polymath-ui/babel.config.js
@@ -1,12 +1,7 @@
 const base = require('../../config/babel.config.js');
 
 module.exports = {
+  babelrcRoots: ['../packages/*'],
   presets: ['react-app'],
-  plugins: [
-    ...base.plugins,
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-transform-runtime',
-    '@babel/plugin-syntax-async-generators',
-    '@babel/plugin-transform-regenerator',
-  ],
+  plugins: [...base.plugins],
 };
