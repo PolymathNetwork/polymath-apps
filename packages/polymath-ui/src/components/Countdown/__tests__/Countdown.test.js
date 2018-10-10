684dbfd690dd807965adddcbb59390c3077e1348
RafaelVidaurre <narzerus@gmail.com>
2018-10-10 00:40:07 -0300

    style(ui): small styling fix

--- packages/polymath-ui/src/components/Countdown/__tests__/Countdown.test.js
+++ packages/polymath-ui/src/components/Countdown/__tests__/Countdown.test.js
@@ -7,8 +7,8 @@ describe('Countdown', () => {
     jest
       .spyOn(Date, 'now')
       .mockImplementation(() => new Date('2018-01-01T05:12:34').getTime());
-    const deadline = new Date('2017-06-23T01:23:45');
 
+    const deadline = new Date('2017-06-23T01:23:45');
     const component = renderer.create(
       <Countdown
         title="Time Left to Create Your Token"
