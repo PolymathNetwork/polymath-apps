b9fd57c71498c06f5520ec89f8080dc831c2d1bc
RafaelVidaurre <narzerus@gmail.com>
2018-10-10 00:57:35 -0300

    test(ui): remove unused setup code

--- packages/polymath-ui/src/specsInit.js
+++ packages/polymath-ui/src/specsInit.js
@@ -1,16 +1,4 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-
-import { uiReducer, networkReducer } from './redux/reducer';
-
-const mockStore = configureMockStore([thunk]);
-
-// NOTE @RafaelVidaurre: Not sure when we'd ever need this
-global.store = mockStore({
-  ui: uiReducer(undefined, {}),
-  network: networkReducer(undefined, {}),
-});
 
 configure({ adapter: new Adapter() });
