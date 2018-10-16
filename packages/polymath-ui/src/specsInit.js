7a0d39faf356c2fd44dbe1882e7a58f9632a38f5
RafaelVidaurre <narzerus@gmail.com>
2018-10-16 18:22:55 -0300

    chore(ui): enable .env.test file for tests

--- packages/polymath-ui/src/specsInit.js
+++ packages/polymath-ui/src/specsInit.js
@@ -1,4 +1,11 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import dotenv from 'dotenv';
+
+const res = dotenv.config({ path: './.env.test' });
+
+if (res.error) {
+  throw res.error;
+}
 
 configure({ adapter: new Adapter() });
