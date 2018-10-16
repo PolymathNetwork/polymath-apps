53d46ec0444d950e2a4eff3241e82e57aca79755
Jeremias Diaz <monitz87@gmail.com>
2018-10-16 03:00:14 -0300

    chore(js): remove exports of deleted modules

--- packages/polymath-js/src/contracts/index.js
+++ packages/polymath-js/src/contracts/index.js
@@ -1,18 +1,11 @@
 // @flow
 
-export { default as CappedSTOFactory } from './CappedSTOFactory';
 export { default as Contract } from './Contract';
 export { default as CountTransferManager } from './CountTransferManager';
-export {
-  default as CountTransferManagerFactory,
-} from './CountTransferManagerFactory';
 export { default as IModuleFactory } from './IModuleFactory';
 export {
   default as PercentageTransferManager,
 } from './PercentageTransferManager';
-export {
-  default as PercentageTransferManagerFactory,
-} from './PercentageTransferManagerFactory';
 export { default as PermissionManager } from './PermissionManager';
 export { default as PolyToken } from './PolyToken';
 export { default as SecurityToken } from './SecurityToken';
