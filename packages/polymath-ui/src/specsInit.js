7e490af5975f15b89933208fab0c9a7e77ca0c0f
RafaelVidaurre <narzerus@gmail.com>
2018-10-16 18:31:31 -0300

    refactor(offchain): isolate env vars in tests

--- packages/polymath-ui/src/specsInit.js
+++ packages/polymath-ui/src/specsInit.js
@@ -2,7 +2,13 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import dotenv from 'dotenv';
 
-const res = dotenv.config({ path: './.env.test' });
+const res = dotenv.config({ path: '.env.test' });
+
+// Override environment variables since we are in test mode and need to isolate them
+process.env = {
+  ...process.env,
+  ...res.parsed,
+};
 
 if (res.error) {
   throw res.error;
