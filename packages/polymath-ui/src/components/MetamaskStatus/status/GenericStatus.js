4f919e049053c71808d77267a734ca80ec1fdc04
Adrien Denat <oi@adriendenat.com>
2018-10-16 18:46:46 -0300

    chore(ui): refactor MetamaskStatus to support new status
    add support for when access is requested by the dapp
    
    # Conflicts:
    #	packages/polymath-ui/src/components/MetamaskStatus/MetamaskStatus.mdx

---
+++ packages/polymath-ui/src/components/MetamaskStatus/status/GenericStatus.js
@@ -0,0 +1,17 @@
+// @flow
+
+import React from 'react';
+
+import metamask from '../../../images/metamask.png';
+
+const GenericStatus = ({ id, title, description }) => (
+  <div id={id} className="pui-single-box-header">
+    <div className="pui-single-box-metamask">
+      <img src={metamask} alt="Metamask" />
+    </div>
+    <h1 className="pui-h1">{title}</h1>
+    <h3 className="pui-h3">{description}</h3>
+  </div>
+);
+
+export default GenericStatus;
