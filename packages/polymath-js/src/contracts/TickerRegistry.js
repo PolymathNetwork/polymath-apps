9898cdab8668ccc5bd2aff65ed090ce99ec73516
RafaelVidaurre <narzerus@gmail.com>
2018-10-16 09:54:51 -0300

    docs(js): leave clarification comment for getMyTokens depracated state

--- packages/polymath-js/src/contracts/TickerRegistry.js
+++ packages/polymath-js/src/contracts/TickerRegistry.js
@@ -73,6 +73,7 @@ class TickerRegistry extends Contract {
 
   /**
    * TODO @bshevchenko: DEPRECATED since owner can be changed
+   * NOTE @RafaelVidaurre: Not really depracated, still being used
    * @deprecated
    */
   async getMyTokens(): Promise<Array<SymbolDetails>> {
