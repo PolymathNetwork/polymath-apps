97afe5271c76cf29f4b0763cc97bb22b53085e20
RafaelVidaurre <narzerus@gmail.com>
2018-10-16 14:12:43 -0300

    chore(shared): setup test env initialization

--- packages/polymath-shared/src/__tests__/utils.js
+++ packages/polymath-shared/src/__tests__/utils.js
@@ -7,29 +7,27 @@ import { getAddressesByNetwork } from '../utils';
  * - In mainnet we want to get production kovan + production mainnet
  */
 
-const ORIGINAL_ENV = process.env;
-
-afterEach(() => {
-  process.env = ORIGINAL_ENV;
-});
+const ORIGINAL_ENV = { ...process.env };
 
 describe('utils', () => {
   beforeEach(() => {
+    console.log(process.env.DEPLOYMENT_STAGE);
+    console.log(process.env.NODE_ENV);
     jest.resetModules();
+  });
+  afterEach(() => {
+    process.env = { ...ORIGINAL_ENV };
   });
 
   describe('getAddressesByNetwork', () => {
     describe('not in local stage', () => {
-      beforeEach(() => {
-        process.env.DEPLOYMENT_STAGE = 'local';
-      });
-
-      test('gets addresses from the NETWORKS constant', () => {
-        console.log(process.env.DEPLOYMENT_STAGE);
-      });
+      test('gets addresses from the NETWORKS constant', () => {});
     });
 
     describe('in local stage', () => {
+      beforeEach(() => {
+        process.env.DEPLOYMENT_STAGE = 'local';
+      });
       test('gets addresses from json artifacts', () => {
         console.log(process.env.DEPLOYMENT_STAGE);
       });
--- packages/polymath-shared/src/specsInit.js
+++ packages/polymath-shared/src/specsInit.js
@@ -1,1 +1,8 @@
 // You can add general test setup code here
+import dotenv from 'dotenv';
+
+const res = dotenv.config({ path: './.env.test' });
+
+if (res.error) {
+  throw res.error;
+}
