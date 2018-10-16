a37e0f6081890f5c68277f8324483f53d39ed11d
Jeremias Diaz <monitz87@gmail.com>
2018-10-16 02:48:36 -0300

    chore(js): delete useless factory contract classes
    These are no longer needed, since factories use the ModuleFactory ABI and addresses are retrieved from the ModuleRegistry

--- packages/polymath-js/src/contracts/PercentageTransferManagerFactory.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import artifact from '@polymathnetwork/shared/fixtures/contracts/PercentageTransferManagerFactory.json';
-
-import IModuleFactory from './IModuleFactory';
-
-class PercentageTransferManagerFactory extends IModuleFactory {}
-
-export default new PercentageTransferManagerFactory(artifact);
