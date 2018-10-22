7b75b22729661929e13d9e73ea08425c68ce1fab
Adrien Denat <oi@adriendenat.com>
2018-10-22 11:50:35 -0300

    feat(ui): add Styled-components default Theme Provider

---
+++ packages/polymath-ui/src/theme.js
@@ -0,0 +1,9 @@
+export default {
+  fontSizes: [12, 14, 16, 18, 20, 24, 28, 32, 36, 42],
+  // Values from: http://www.carbondesignsystem.com/guidelines/spacing/#spacing-scale
+  space: [2, 4, 8, 12, 16, 24, 32, 40, 48, 64, 96, 160],
+  gutter: [10, 20],
+  colors: {
+    gray: ['#EBF0F7', '#8C9BA5', '#5A6872'],
+  },
+};
