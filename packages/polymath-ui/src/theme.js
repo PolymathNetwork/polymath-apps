9c2bffd1c5b0cb320f05ef81d0383108499471d5
Adrien Denat <oi@adriendenat.com>
2018-10-22 19:32:20 -0300

    feat(ui): add headings variant styles

--- packages/polymath-ui/src/theme.js
+++ packages/polymath-ui/src/theme.js
@@ -1,9 +1,23 @@
-export default {
-  fontSizes: [12, 14, 16, 18, 20, 24, 28, 32, 36, 42],
-  // Values from: http://www.carbondesignsystem.com/guidelines/spacing/#spacing-scale
-  space: [2, 4, 8, 12, 16, 24, 32, 40, 48, 64, 96, 160],
-  gutter: [10, 20],
-  colors: {
-    gray: ['#EBF0F7', '#8C9BA5', '#5A6872'],
+const fontSizes = [12, 14, 16, 18, 20, 24, 28, 32, 36, 42];
+const space = [0, 5, 10, 20, 30, 50, 80, 120];
+const colors = {
+  baseTextColor: '#152935',
+  gray: ['#EBF0F7', '#8C9BA5', '#5A6872'],
+};
+
+const headings = {
+  h1: {},
+  h2: {},
+  h3: {
+    fontSize: `${fontSizes[4]}px`,
+    fontWeight: 600,
+    marginBottom: `${space[4]}px`,
   },
+};
+
+export default {
+  fontSizes,
+  space,
+  colors,
+  headings,
 };
