e6a3e37ebe46b04d89d0e2f5c2ad371eccc9c360
Jeremias Diaz <monitz87@gmail.com>
2018-10-22 01:24:19 -0300

    build(shared): remove redundant plugin

--- packages/polymath-shared/.babelrc.js
+++ packages/polymath-shared/.babelrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: ['@babel/flow', '@babel/env'],
   plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-async-generators',
     '@babel/plugin-transform-regenerator',
