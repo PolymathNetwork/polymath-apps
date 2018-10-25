8803f8cf32f5e091bc8c10bce7646c5aaef121fe
Adrien Denat <oi@adriendenat.com>
2018-10-25 01:21:16 -0300

    feat(ui): add a custom Playground Docz component
    to reset global CSS applied by Docz

--- packages/polymath-ui/src/components/DoczWrapper.js
+++ packages/polymath-ui/src/components/DoczWrapper.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-// import { Provider } from 'react-redux';
-// import { createStore, combineReducers } from 'redux';
-
-// TODO @grsmto: we can't include this wrapper before it imports some polymath-js dependencies that break Docz build. Need to investigate why this is happening.
-
-// import { uiReducer, networkReducer } from '../redux/reducer';
 
 import '../styles/globals.scss';
 
 import theme from '../theme';
 
-// const reducers = combineReducers({ ui: uiReducer, network: networkReducer });
-// const store = createStore(reducers);
-
-// const Wrapper = ({ children }) => <Provider store={store}>{children}</Provider>;
 const Wrapper = ({ children }) => (
   <ThemeProvider theme={theme}>{children}</ThemeProvider>
 );
