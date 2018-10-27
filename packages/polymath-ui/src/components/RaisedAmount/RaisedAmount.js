7ed65fd6de04743e42e6c561a07b757513d8055f
Adrien Denat <oi@adriendenat.com>
2018-10-27 00:23:36 -0300

    fix(ui): fix styling of RaisedAmount component

--- packages/polymath-ui/src/components/RaisedAmount/RaisedAmount.js
+++ packages/polymath-ui/src/components/RaisedAmount/RaisedAmount.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import Paragraph from '../Paragraph';
 
 import './style.scss';
 
+const Container = styled.div`
+  padding: 10px 15px;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.colors.blue[0]};
+`;
+
+const LightText = styled.span`
+  color: ${({ theme }) => theme.colors.gray[2]};
+`;
+
 const RaisedAmount = ({ title, primaryAmount, tokenAmount }) => (
-  <div className="pui-key-value pui-countdown-raised">
-    <div>{title}</div>
-    {primaryAmount}
-    <div>{tokenAmount}</div>
-  </div>
+  <Container>
+    <Paragraph as="h4" fontSize={1} mb={3}>
+      {title}
+    </Paragraph>
+    <Paragraph fontSize={5} mb={2}>
+      {primaryAmount}
+    </Paragraph>
+    <LightText>{tokenAmount}</LightText>
+  </Container>
 );
 
 export default RaisedAmount;
