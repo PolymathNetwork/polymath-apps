9ac2812a60b2496050eacc94f2b7d8c9f1a7cb5b
RafaelVidaurre <narzerus@gmail.com>
2018-11-08 10:59:10 -0300

    feat(shared): export DAI addresses

--- packages/polymath-shared/src/constants.js
+++ packages/polymath-shared/src/constants.js
@@ -6,3 +6,12 @@ export const LOCAL_NETWORK_ID = '15';
 export const LOCALVM_NETWORK_ID = '16';
 export const ROPSTEN_NETWORK_ID = '3';
 export const RINKEBY_NETWORK_ID = '4';
+
+// NOTE @RafaelVidaurre: Uses the DAI's kovan address for local network for
+// simplicity
+export const DAI_ADDRESSES = {
+  [MAINNET_NETWORK_ID]: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+  [KOVAN_NETWORK_ID]: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2',
+  [LOCAL_NETWORK_ID]: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2',
+  [LOCALVM_NETWORK_ID]: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2',
+};
