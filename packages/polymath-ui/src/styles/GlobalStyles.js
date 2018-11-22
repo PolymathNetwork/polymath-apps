874b4df7dce82b05f233d9c869d0bc1480009933
Adrien Denat <adrien.denat@gmail.com>
2018-11-22 18:52:51 -0200

    fix(ui): fix Docz not working

--- packages/polymath-ui/src/styles/GlobalStyles.js
+++ packages/polymath-ui/src/styles/GlobalStyles.js
@@ -1,10 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
-import 'typeface-overpass';
+import fontFaceDefinition from 'typeface-overpass';
 
 import '../deprecated/styles/globals.scss';
 
 const GlobalStyles = createGlobalStyle`
+  ${fontFaceDefinition};
+
   body {
     color: ${({ theme }) => theme.colors.baseText};
     font-family: 'Overpass', sans-serif;
