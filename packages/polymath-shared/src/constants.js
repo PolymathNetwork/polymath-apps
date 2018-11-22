b8f7f4a2cc537f1b7b59b71b51b3ec2440e21175
Jeremias Diaz <monitz87@gmail.com>
2018-11-22 17:20:28 -0300

    feat(shared): update local blockchain

--- packages/polymath-shared/src/constants.js
+++ packages/polymath-shared/src/constants.js
@@ -1,5 +1,7 @@
 // @flow
 
+import PolyTokenFaucetArtifact from './fixtures/contracts/PolyTokenFaucet.json';
+
 export const KOVAN_NETWORK_ID = '42';
 export const MAINNET_NETWORK_ID = '1';
 export const LOCAL_NETWORK_ID = '15';
@@ -10,13 +12,18 @@ export const RINKEBY_NETWORK_ID = '4';
 type DaiAddresses = {
   [networkId: string]: string,
 };
-// NOTE @RafaelVidaurre: Uses the DAI's kovan address for local network for
-// simplicity
+
+/**
+ * NOTE @monitz87: uses a second deployed PolyTokenFaucet as DAI for local networks
+ * (they are compatible as they are both ERC20 tokens)
+ */
+const faucetAddress =
+  PolyTokenFaucetArtifact.networks[LOCAL_NETWORK_ID].address;
 export const DAI_ADDRESSES: DaiAddresses = {
   [MAINNET_NETWORK_ID]: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
   [KOVAN_NETWORK_ID]: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2',
-  [LOCAL_NETWORK_ID]: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2',
-  [LOCALVM_NETWORK_ID]: '0xc4375b7de8af5a38a93548eb8453a498222c4ff2',
+  [LOCAL_NETWORK_ID]: faucetAddress,
+  [LOCALVM_NETWORK_ID]: faucetAddress,
 };
 
 export const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
