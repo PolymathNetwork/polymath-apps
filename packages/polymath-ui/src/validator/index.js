82da3b557c3adf425021fa3ea1f17be4f7156ed7
Jeremias Diaz <monitz87@gmail.com>
2018-11-26 17:16:37 -0300

    feat(ui): add email validator

--- packages/polymath-ui/src/validator/index.js
+++ packages/polymath-ui/src/validator/index.js
@@ -20,4 +20,16 @@ Yup.addMethod(Yup.string, 'isAddress', function(message) {
   });
 });
 
+Yup.addMethod(Yup.string, 'isEmail', function(message) {
+  const emailRegex = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z0-9-]+$/i;
+
+  return this.test('validateIsEmail', function(value) {
+    return emailRegex.test(value)
+      ? true
+      : this.createError({
+          message,
+        });
+  });
+});
+
 export default Yup;
