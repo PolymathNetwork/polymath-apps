1d73bfde95901b15b236365096df9f8f6d46cb83
Adrien Denat <adrien.denat@gmail.com>
2018-11-30 16:45:59 -0200

    feat(ui): add new GridRow/GridCol components

---
+++ packages/polymath-ui/src/components/Grid/GridRow/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+import Grid from '../Grid';
+
+const GridRow = styled(Grid)`
+  grid-template-columns: repeat(12, 1fr);
+`;
+
+export default GridRow;
--- packages/polymath-ui/src/components/Grid/index.js
+++ packages/polymath-ui/src/components/Grid/index.js
@@ -1,6 +1,10 @@
 import Grid from './Grid';
 import GridItem from './GridItem';
+import GridRow from './GridRow';
+import GridCol from './GridCol';
 
 export default Object.assign(Grid, {
   Item: GridItem,
+  Row: GridRow,
+  Col: GridCol,
 });
