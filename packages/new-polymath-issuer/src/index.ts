328062e847e999f15671b160bc03996027f0bc3d
RafaelVidaurre <narzerus@gmail.com>
2018-12-03 18:30:54 -0300

    chore: initialize webpack config

---
+++ packages/new-polymath-issuer/src/index.ts
@@ -0,0 +1,3 @@
+import { Foo } from '@polymathnetwork/new-ui';
+
+console.log('Foo', Foo);
---
+++ packages/new-polymath-issuer/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const DeclarationBundlerPlugin = require('declaration-bundler-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          projectReferences: true,
+          compiler: 'ttypescript',
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts'],
+    plugins: [
+      // new TsconfigPathsPlugin(),
+      new DeclarationBundlerPlugin({
+        moduleName: '@polymathnetwork/new-issuer',
+      }),
+    ],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build/dist'),
+  },
+};
