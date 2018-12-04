9dcf4eb5b9d015d4c4b14124cca18b33937b843c
Adrien Denat <adrien.denat@gmail.com>
2018-12-04 18:07:36 -0200

    chore(ui): export missing Icon components + new icon

---
+++ packages/polymath-ui/src/images/icons/Download.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SvgDownload = props => (
+  <svg width="1em" height="1em" {...props}>
+    <g fillRule="evenodd">
+      <path d="M12.667 6.267l-.8-.734-3.2 3.134V0H7.333v8.667l-3.2-3.134-.8.734L8 10.667z" />
+      <path d="M14.667 9.333v4H1.333v-4H0V16h16V9.333z" />
+    </g>
+  </svg>
+);
+
+export default SvgDownload;
