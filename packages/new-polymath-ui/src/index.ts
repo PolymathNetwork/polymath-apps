dc2aa4afe8c9e462580da0e0fb794cbc4e67c49e
RafaelVidaurre <narzerus@gmail.com>
2018-12-06 09:13:48 -0300

    chore: correct bad export

--- packages/new-polymath-ui/src/index.ts
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-// import { Foo } from '~/components/test-module';
-import { Foo } from './Foo';
-export { Foo };
-export class ImportedTestComponent extends Component<{ text: string }> {
-  public render() {
-    return <p>{this.props.text}</p>;
-  }
-}
