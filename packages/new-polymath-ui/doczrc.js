65ed6ea0431f217f7c2edce30de4010abd7c659c
Adrien Denat <adrien.denat@gmail.com>
2018-12-10 11:35:51 -0200

    fix(new-ui): try to filter out leaking props of PropsTable

--- packages/new-polymath-ui/doczrc.js
+++ packages/new-polymath-ui/doczrc.js
@@ -1,3 +1,4 @@
+import { set } from 'lodash';
 import { css } from 'docz-plugin-css';
 
 export default {
@@ -14,4 +15,27 @@ export default {
       preprocessor: 'sass',
     }),
   ],
+  modifyBundlerConfig: config => {
+    const jsxPluginIndex = config.plugins.findIndex(
+      plugin => plugin.config.id === 'jsx'
+    );
+    const { loaders } = config.plugins[jsxPluginIndex].config;
+    const docGenLoaderIndex = loaders.findIndex(loader =>
+      /react-docgen-typescript-loader/.test(loader.loader)
+    );
+    const docGenLoader = loaders[docGenLoaderIndex];
+
+    set(docGenLoader, 'options.propFilter', prop => {
+      if (prop.parent == null) {
+        return true;
+      }
+
+      // Filter out props which type definition is placed in react package
+      return prop.parent.fileName.indexOf('node_modules/@types/react') < 0;
+    });
+
+    set(docGenLoader, 'options.tsconfigPath', './tsconfig.json');
+
+    return config;
+  },
 };
