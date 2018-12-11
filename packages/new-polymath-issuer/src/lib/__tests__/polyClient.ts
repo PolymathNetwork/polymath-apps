4da5d67a0386f6b9ff44275ad6deb00833097f36
RafaelVidaurre <narzerus@gmail.com>
2018-12-11 17:32:30 -0300

    build(issuer): fix test runner

--- packages/new-polymath-issuer/src/lib/__tests__/polyClient.ts
+++ packages/new-polymath-issuer/src/lib/__tests__/polyClient.ts
@@ -2,7 +2,7 @@ import { polyClient } from '~/lib/polyClient';
 import { PolymathClient } from '@polymathnetwork/sdk';
 
 describe('polyClient', () => {
-  test('client is instanciated', () => {
+  test('client is instanciated at design time', () => {
     expect(polyClient instanceof PolymathClient).toBeTruthy();
   });
 });
---
+++ packages/new-polymath-issuer/src/testUtils/setupTests.tsx
@@ -0,0 +1,21 @@
+import '@babel/polyfill';
+
+import React, { Fragment } from 'react';
+import { render } from 'react-testing-library';
+import configureMockStore from 'redux-mock-store';
+
+const middlewares: any[] = [];
+
+const customRender = (node: React.ReactElement<any>, ...opts: any[]) => {
+  const { rerender, ...result } = render(<Fragment>{node}</Fragment>, ...opts);
+
+  const customRerender = (rerenderNode: React.ReactElement<any>) => {
+    return rerender(<Fragment>{rerenderNode}</Fragment>);
+  };
+
+  return { rerender: customRerender, ...result };
+};
+
+export const mockStore = configureMockStore(middlewares);
+export * from 'react-testing-library';
+export { customRender as render };
