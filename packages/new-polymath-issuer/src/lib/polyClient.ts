c515f286d89c86f2ce748eb5822888677649a677
RafaelVidaurre <narzerus@gmail.com>
2018-12-12 13:15:32 -0300

    refactor(new-issuer): polyClient instanciation

--- packages/new-polymath-issuer/src/lib/polyClient.ts
+++ packages/new-polymath-issuer/src/lib/polyClient.ts
@@ -5,14 +5,29 @@ import {
   POLYMATH_REGISTRY_ADDRESS_KOVAN,
 } from '~/constants';
 
-const params = {
-  addresses: {
-    [constants.NetworkIds.Local]: {
-      polymathRegistryAddress: POLYMATH_REGISTRY_ADDRESS_LOCAL,
-    },
+let kovanConfig = {};
+let localConfig = {};
+
+if (POLYMATH_REGISTRY_ADDRESS_KOVAN) {
+  kovanConfig = {
     [constants.NetworkIds.Kovan]: {
       polymathRegistryAddress: POLYMATH_REGISTRY_ADDRESS_KOVAN,
     },
+  };
+}
+
+if (POLYMATH_REGISTRY_ADDRESS_LOCAL) {
+  localConfig = {
+    [constants.NetworkIds.Local]: {
+      polymathRegistryAddress: POLYMATH_REGISTRY_ADDRESS_LOCAL,
+    },
+  };
+}
+
+const params = {
+  addresses: {
+    ...localConfig,
+    ...kovanConfig,
   },
 };
 export const polyClient = new PolymathClient(params);
