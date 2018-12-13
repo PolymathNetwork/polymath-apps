4a1b1de7b060988846b8d3789df4b6d2e0f310ad
Adrien Denat <adrien.denat@gmail.com>
2018-12-13 19:18:19 -0200

    chore(new-ui): add styling on TextArea

--- packages/new-polymath-ui/src/components/inputs/TextArea/TextArea.tsx
+++ packages/new-polymath-ui/src/components/inputs/TextArea/TextArea.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 import { formikProxy } from '../formikProxy';
 
@@ -6,6 +7,17 @@ export interface TextAreaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   error?: string;
 }
+
+const StyledTextArea = styled.textarea`
+  width: 100%;
+  min-width: 10rem;
+  padding: 1rem;
+  font-size: ${({ theme }) => theme.fontSizes.baseText};
+  color: ${({ theme }) => theme.colors.baseText};
+  font-family: ${({ theme }) => theme.fontFamilies.baseText};
+  background-color: ${({ theme }) => theme.colors.gray[1]};
+  border: none;
+`;
 
 export class TextAreaPrimitive extends Component<TextAreaProps> {
   handleChange = event => {
@@ -19,7 +31,7 @@ export class TextAreaPrimitive extends Component<TextAreaProps> {
     const { onChange, error, ...textareaProps } = this.props;
 
     const input = error ? (
-      <textarea
+      <StyledTextArea
         onChange={this.handleChange}
         {...textareaProps}
         aria-invalid
@@ -27,7 +39,7 @@ export class TextAreaPrimitive extends Component<TextAreaProps> {
         data-invalid
       />
     ) : (
-      <textarea onChange={this.handleChange} {...textareaProps} />
+      <StyledTextArea onChange={this.handleChange} {...textareaProps} />
     );
 
     return input;
