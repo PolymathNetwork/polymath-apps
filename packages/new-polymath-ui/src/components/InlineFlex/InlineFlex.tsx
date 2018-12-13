d7619a11cac054039d63ce9ff1263174efea1986
Adrien Denat <adrien.denat@gmail.com>
2018-12-13 19:14:04 -0200

    chore(new-ui): add missing components

---
+++ packages/new-polymath-ui/src/components/InlineFlex/InlineFlex.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Box } from '../Box';
+
+export const InlineFlex = styled(Box)`
+  display: inline-flex;
+  align-items: center;
+`;
