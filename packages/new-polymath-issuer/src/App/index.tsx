f5731085c1ed93dd2625b4a33d07915d3dc03152
RafaelVidaurre <narzerus@gmail.com>
2018-12-17 15:42:32 -0300

    fix(new-issuer): outdated store import

--- packages/new-polymath-issuer/src/App/index.tsx
+++ packages/new-polymath-issuer/src/App/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import { getStore } from '~/state/store';
+import { store } from '~/state/store';
 import { Pages } from '~/pages';
 
 export const App = () => (
-  <Provider store={getStore()}>
+  <Provider store={store}>
     <Pages />
   </Provider>
 );
