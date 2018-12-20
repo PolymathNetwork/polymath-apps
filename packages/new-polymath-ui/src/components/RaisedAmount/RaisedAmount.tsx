9a63b4c76a8ca6659e46b95998d6fc44fe83aa7b
Adrien Denat <adrien.denat@gmail.com>
2018-12-20 10:31:29 -0200

    feat(new-ui): add RaiseAmount component

---
+++ packages/new-polymath-ui/src/components/RaisedAmount/RaisedAmount.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+import styled from 'styled-components';
+import { formatters } from '@polymathnetwork/new-shared';
+
+import { Paragraph } from '../Paragraph';
+
+const Container = styled.div`
+  padding: 10px 15px;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.colors.gray[1]};
+  min-width: 250px;
+`;
+
+interface RaisedAmountProps {
+  title: string;
+  primaryAmount: number;
+  primaryUnit: string;
+  tokenAmount: number;
+  tokenUnit: string;
+}
+
+export const RaisedAmount: FC<RaisedAmountProps> = ({
+  title,
+  primaryAmount,
+  primaryUnit,
+  tokenAmount,
+  tokenUnit,
+}) => (
+  <Container>
+    <Paragraph as="h4" fontSize={1} color="highlightText" mb={1}>
+      {title}
+    </Paragraph>
+    <Paragraph fontSize={5} color="highlightText" mb={1}>
+      {`${formatters.thousandsDelimiter(primaryAmount)} ${primaryUnit}`}
+    </Paragraph>
+    {`${formatters.thousandsDelimiter(tokenAmount)} ${tokenUnit}`}
+  </Container>
+);
