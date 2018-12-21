81f3782257b52f6945aa4c5775a59e2b807eb307
Jeremias Diaz <monitz87@gmail.com>
2018-12-21 01:43:31 -0300

    feat(new-issuer): create new entities and improve typing

--- packages/new-polymath-issuer/src/state/reducers/entities.ts
+++ packages/new-polymath-issuer/src/state/reducers/entities.ts
@@ -1,7 +1,23 @@
-import { createEntityReducer } from '~/state/helpers/createEntityReducer';
-import { Transaction } from '~/types';
+import {
+  createEntityReducer,
+  EntityState,
+} from '~/state/helpers/createEntityReducer';
+import {
+  TransactionEntity,
+  DividendEntity,
+  CheckpointEntity,
+  Entities,
+} from '~/types';
 import { combineReducers } from 'redux';
 
-export const reducer = combineReducers({
-  transactions: createEntityReducer<Transaction>('TRANSACTIONS'),
+export interface EntitiesState {
+  transactions: EntityState<TransactionEntity>;
+  checkpoints: EntityState<CheckpointEntity>;
+  dividends: EntityState<DividendEntity>;
+}
+
+export const reducer = combineReducers<EntitiesState>({
+  transactions: createEntityReducer<TransactionEntity>('TRANSACTIONS'),
+  checkpoints: createEntityReducer<CheckpointEntity>('CHECKPOINTS'),
+  dividends: createEntityReducer<DividendEntity>('DIVIDENDS'),
 });
