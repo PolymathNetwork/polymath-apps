04960b0d155303c2d5fdfb436d6f9c9f4e730cbc
Jeremias Diaz <monitz87@gmail.com>
2018-12-21 01:32:12 -0300

    feat(new-issuer): create new types

--- packages/new-polymath-issuer/src/types/index.ts
+++ packages/new-polymath-issuer/src/types/index.ts
@@ -1,6 +1,39 @@
-export interface Transaction {
+import BigNumber from 'bignumber.js';
+
+export type Entity = TransactionEntity | DividendEntity | CheckpointEntity;
+
+export interface TransactionEntity {
   id: string;
   txHash: string;
+}
+
+export interface DividendEntity {
+  id: string;
+  index: number;
+  checkpointId: string;
+  created: Date;
+  maturity: Date;
+  expiry: Date;
+  amount: BigNumber;
+  claimedAmount: BigNumber;
+  totalSupply: BigNumber;
+  reclaimed: boolean;
+  dividendWithheld: BigNumber;
+  dividendWithheldReclaimed: BigNumber;
+  name: string;
+  currency: string;
+}
+
+export interface CheckpointEntity {
+  id: string;
+  index: number;
+  securityTokenId: string;
+  investorBalances: Array<{
+    address: string;
+    balance: BigNumber;
+  }>;
+  totalSupply: BigNumber;
+  createdAt: Date;
 }
 
 export interface Wallet {
@@ -18,4 +51,35 @@ export enum SessionRoles {
   LoggedIn,
   UnconfirmedUser,
   ConfirmedUser,
+}
+
+export enum Entities {
+  Checkpoints = 'checkpoints',
+  Transactions = 'transactions',
+  Dividends = 'dividends',
+}
+
+export enum RequestKeys {
+  GetCheckpointsBySymbol = 'getCheckpointsBySymbol',
+}
+
+export interface Fetcher {
+  propKey?: string;
+  entity: Entities;
+  requestKey: RequestKeys;
+  args: Pojo;
+}
+
+export interface Pojo {
+  [key: string]: string | number | boolean;
+}
+
+export interface FetchedData {
+  [key: string]: Entity[] | null | undefined;
+}
+
+export interface CacheStatus {
+  args: Pojo;
+  requestKey: RequestKeys;
+  mustBeFetched: boolean;
 }
