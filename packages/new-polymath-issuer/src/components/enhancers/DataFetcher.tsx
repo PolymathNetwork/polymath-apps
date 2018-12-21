335abf247963236f0e081cd9565993c15c3e56f9
Jeremias Diaz <monitz87@gmail.com>
2018-12-21 01:49:02 -0300

    feat(new-issuer): implement the DataFetcher component

---
+++ packages/new-polymath-issuer/src/components/enhancers/DataFetcher.tsx
@@ -0,0 +1,88 @@
+import { connect } from 'react-redux';
+import { RootState } from '~/state/store';
+import { Fetcher, CacheStatus, FetchedData } from '~/types';
+import { Component, Dispatch, ReactNode } from 'react';
+import {
+  createGetEntitiesFromCache,
+  createGetCacheStatus,
+} from '~/state/selectors';
+import { Loading } from '~/components/__temporary';
+import React from 'react';
+import _ from 'lodash';
+
+interface OwnProps {
+  dispatch: Dispatch<any>;
+  fetchers: Fetcher[];
+  render(props: FetchedData): ReactNode;
+}
+
+interface StateProps {
+  fetchedData: FetchedData;
+  cache: CacheStatus[];
+  loading: boolean;
+}
+
+type Props = OwnProps & StateProps;
+
+const mapStateToProps = () => {
+  const entitiesSelector = createGetEntitiesFromCache();
+  const cacheSelector = createGetCacheStatus();
+
+  return (state: RootState, props: OwnProps): StateProps => {
+    const fetchedData = entitiesSelector(state, props);
+    const unfilteredCache = cacheSelector(state, props);
+
+    const cache = _.filter(
+      unfilteredCache,
+      cacheStatus => cacheStatus.mustBeFetched
+    );
+    const loading = !!cache.length;
+
+    return {
+      fetchedData,
+      cache,
+      loading,
+    };
+  };
+};
+
+interface InternalState {
+  loading: boolean;
+}
+
+class DataFetcherBase extends Component<Props, InternalState> {
+  public state = {
+    loading: false,
+  };
+
+  public getData() {
+    const { dispatch, cache } = this.props;
+    _.forEach(cache, cacheStatus => {
+      /**
+       * TODO @monitz87: dispatch the action that starts fetching.
+       * This action should trigger a saga that delegates the actual request
+       * to other sagas depending on the requestKey
+       */
+    });
+  }
+
+  public componentDidMount() {
+    this.getData();
+  }
+
+  public componentWillUpdate() {
+    this.getData();
+  }
+
+  public render() {
+    const { render, fetchedData, loading } = this.props;
+
+    if (loading) {
+      return <Loading />;
+    }
+
+    return render(fetchedData);
+  }
+}
+
+export const DataFetcher = connect(mapStateToProps)(DataFetcherBase);
