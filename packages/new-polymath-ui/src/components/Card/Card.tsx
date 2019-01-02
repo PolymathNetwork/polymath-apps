ac654033cc1ccd51f04e61d3de368748b6e01bc0
Adrien Denat <adrien.denat@gmail.com>
2019-01-02 21:04:55 -0200

    refactor(new-ui): rename ContentBox component to Card

--- packages/new-polymath-ui/src/components/Card/Card.tsx
+++ packages/new-polymath-ui/src/components/Card/Card.tsx
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 
 import { Box, BoxProps } from '../Box';
 
-export interface ContentBoxProps extends BoxProps {}
+export interface CardProps extends BoxProps {}
 
-export const ContentBox = styled(Box)<ContentBoxProps>`
+export const Card = styled(Box)<CardProps>`
   background-color: white;
   box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
   padding: ${({ theme }) => theme.space[6]};
 `;
 
 // TODO @grsmto: remove when https://github.com/pedronauck/docz/issues/337 is resolved
-export const ContentBoxDocz = (props: ContentBoxProps) => {
-  return <ContentBox {...props} />;
+export const CardDocz = (props: CardProps) => {
+  return <Card {...props} />;
 };
