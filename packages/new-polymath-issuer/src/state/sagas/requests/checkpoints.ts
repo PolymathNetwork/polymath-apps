2244a460e0265aebee25d893b577e5eeec313de4
Jeremias Diaz <monitz87@gmail.com>
2019-01-02 22:39:02 -0300

    feat(new-issuer): WIP implement checkpoint request sagas

--- packages/new-polymath-issuer/src/state/sagas/requests/checkpoints.ts
+++ packages/new-polymath-issuer/src/state/sagas/requests/checkpoints.ts
@@ -1,42 +1,40 @@
 import { polyClient } from '~/lib/polymath';
-import {
-  fetchCheckpoints as fetch,
-  fetchCheckpointsSuccess,
-  fetchCheckpointsError,
-} from '~/state/actions/checkpoints';
+import { cacheData, fetchDataError } from '~/state/actions/dataRequests';
+import { createAction as createCheckpoint } from '~/state/actions/checkpoints';
+import { createAction as createDividend } from '~/state/actions/dividends';
 import { takeLatest } from 'redux-saga';
 import { getType, ActionType } from 'typesafe-actions';
 import { call, put } from 'redux-saga/effects';
 import { Checkpoint } from '@polymathnetwork/sdk';
 import BigNumber from 'bignumber.js';
+import { RequestKeys } from '~/types';
 
-export function* fetchCheckpoints(action: ActionType<typeof fetch>) {
-  const symbol = action.payload;
-
+export function* fetchCheckpoints(args: { symbol: string }) {
   try {
-    const checkpoints: Checkpoint[] = yield call(polyClient.getCheckpoints, {
-      symbol,
-    });
-    // const checkpoints: Checkpoint[] = [
-    //   {
-    //     id: 1,
-    //     dividends: [],
-    //     totalSupply: new BigNumber('2000'),
-    //     createdAt: new Date(),
-    //     investorBalances: [
-    //       {
-    //         address: '0x1',
-    //         balance: new BigNumber('1000'),
-    //       },
-    //     ],
-    //   },
-    // ];
-    yield put(fetchCheckpointsSuccess(checkpoints));
-  } catch (err) {
-    yield put(fetchCheckpointsError(err));
-  }
-}
+    // const checkpoints: Checkpoint[] = yield call(polyClient.getCheckpoints, {
+    //   symbol,
+    // });
+    const checkpoints: Checkpoint[] = [
+      {
+        id: 1,
+        dividends: [],
+        totalSupply: new BigNumber('2000'),
+        createdAt: new Date(),
+        investorBalances: [
+          {
+            address: '0x1',
+            balance: new BigNumber('1000'),
+          },
+        ],
+      },
+    ];
 
-export function* checkpointsWatcher() {
-  yield takeLatest(getType(fetch), fetchCheckpoints);
+    // yield put(cacheData({
+    //   requestKey: RequestKeys.GetCheckpointsBySymbol,
+    //   args,
+    //   fetchedIds: ['1']
+    // }));
+  } catch (err) {
+    yield put(fetchDataError(err));
+  }
 }
--- packages/new-polymath-issuer/src/state/sagas/requests/index.ts
+++ packages/new-polymath-issuer/src/state/sagas/requests/index.ts
@@ -1,6 +1,29 @@
-import { checkpointsWatcher } from './checkpoints';
-import { all } from 'redux-saga/effects';
+import { call, takeLatest } from 'redux-saga/effects';
+import { fetchData } from '~/state/actions/dataRequests';
+import { getType, ActionType } from 'typesafe-actions';
+import { RequestKeys } from '~/types';
+import { fetchCheckpoints } from '~/state/sagas/requests/checkpoints';
+
+export function* requestData(action: ActionType<typeof fetchData>) {
+  const {
+    payload: { requestKey, args },
+  } = action;
+
+  switch (requestKey) {
+    case RequestKeys.GetCheckpointsBySymbol: {
+      if (typeof args.symbol === 'string') {
+        yield call(fetchCheckpoints, args as { symbol: string });
+      } else {
+        throw new Error(
+          'Invalid arguments passed for fetching checkpoints. Missing token symbol.'
+        );
+      }
+    }
+    default:
+      return;
+  }
+}
 
 export function* requestWatcher() {
-  yield all([checkpointsWatcher()]);
+  yield takeLatest(getType(fetchData), requestData);
 }
