2343b9ab25df02a4a033862cd1b632fa155b7b2c
Jeremias Diaz <monitz87@gmail.com>
2019-01-03 13:09:13 -0300

    fix(sdk): make entity error message more generic

--- packages/new-polymath-sdk/src/entities/Entity.ts
+++ packages/new-polymath-sdk/src/entities/Entity.ts
@@ -6,7 +6,7 @@ export class Entity {
   constructor(polyClient?: Polymath) {
     if (!polyClient) {
       throw new Error(
-        'SecurityToken entity class should always be initialized through the Polymath client'
+        'Entity class should always be initialized through the Polymath client'
       );
     }
 
