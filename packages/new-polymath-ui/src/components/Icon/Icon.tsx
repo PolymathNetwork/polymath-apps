814c5cdb27f9ae10dd108600260423d610665e8f
Adrien Denat <adrien.denat@gmail.com>
2019-01-04 15:00:29 -0200

    chore(new-ui): icon component supports Color prop

--- packages/new-polymath-ui/src/components/Icon/Icon.tsx
+++ packages/new-polymath-ui/src/components/Icon/Icon.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled, { StyledProps } from 'styled-components';
+import { color, ColorProps } from 'styled-system';
 
 export interface IconProps extends StyledProps<any> {
+  color: ColorProps;
   Asset: React.ComponentType<React.SVGAttributes<SVGElement>>;
 }
 
-export const Icon = styled(({ Asset, ...props }: IconProps) => (
+export const Icon = styled(({ Asset, color, ...props }: IconProps) => (
   <Asset {...props} />
 ))`
   vertical-align: middle;
+  ${color};
 `;
 
 // TODO @grsmto: remove when https://github.com/pedronauck/docz/issues/337 is resolved
