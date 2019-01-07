5810bb9d1bb025c91b8e312c36a24ece27504404
Adrien Denat <adrien.denat@gmail.com>
2019-01-07 15:20:25 -0200

    chore(new-ui): remove unused SVGR icons compiled files

--- packages/new-polymath-ui/src/images/icons/Dot.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-export const SvgDot = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="1em" height="1em" {...props}>
-    <circle fill="currentColor" cx={12} cy={12} r={6} />
-  </svg>
-);
--- packages/new-polymath-ui/src/images/icons/Plus.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-export const SvgPlus = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="1em" height="1em" {...props}>
-    <path d="M7 0H5v5H0v2h5v5h2V7h5V5H7z" fillRule="evenodd" />
-  </svg>
-);
