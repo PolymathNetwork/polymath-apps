09e872bb40c942d0bde4ea35ebc1d12be11e504c
Jeremias Diaz <monitz87@gmail.com>
2019-01-07 16:20:00 -0300

    feat(sdk): implement the Erc20DividendsModule entity

--- packages/new-polymath-sdk/src/entities/index.ts
+++ packages/new-polymath-sdk/src/entities/index.ts
@@ -1,8 +1,15 @@
 import { SecurityTokenReservation } from './SecurityTokenReservation';
 import { SecurityToken } from './SecurityToken';
+import { Erc20DividendsModule } from './Erc20DividendsModule';
 import { Dividend } from './Dividend';
 import { Checkpoint } from './Checkpoint';
 
 // export type Entity = typeof SecurityTokenReservation | typeof SecurityToken;
 
-export { SecurityToken, SecurityTokenReservation, Checkpoint, Dividend };
+export {
+  SecurityToken,
+  SecurityTokenReservation,
+  Checkpoint,
+  Dividend,
+  Erc20DividendsModule,
+};
