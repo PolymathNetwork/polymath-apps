3d1a427200764c116c0f5c81359acad1cc3c98e1
Adrien Denat <adrien.denat@gmail.com>
2019-01-07 14:59:53 -0200

    refactor(new-ui): remove circle around icons to avoid duplications

--- packages/new-polymath-ui/src/images/icons/Dai.tsx
+++ packages/new-polymath-ui/src/images/icons/Dai.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 export const SvgDai = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="1em" height="1em" {...props}>
-    <g fill="none" fillRule="evenodd">
-      <circle fill="#FFEFC4" cx={12.5} cy={12.5} r={12.5} />
-      <path
-        d="M12.5 4.5l8 8-8 8-8-8 8-8zm-5.222 7.477h3.655l1.567-1.74 1.74 1.74h3.656L12.5 6.147l-5.222 5.83z"
-        fill="#FEBE44"
-        mask="url(#dai_svg__b)"
-      />
-    </g>
+  <svg width="1em" height="1em" viewBox="0 0 17 17" {...props}>
+    <path
+      d="M8.5.5l8 8-8 8-8-8 8-8zM3.278 7.977h3.655L8.5 6.237l1.74 1.74h3.656L8.5 2.147l-5.222 5.83z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
   </svg>
 );
--- packages/new-polymath-ui/src/images/icons/Eth.tsx
+++ packages/new-polymath-ui/src/images/icons/Eth.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 export const SvgEth = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="1em" height="1em" {...props}>
-    <circle fill="#EED3FE" cx={12.5} cy={12.5} r={12.5} />
-    <g fill="#724396">
-      <path d="M12.808 5L8 13l4.808 2.857L17.615 13z" />
-      <path d="M8 13.952l4.808 6.667 4.807-6.667-4.807 2.858z" />
-    </g>
+  <svg width="1em" height="1em" viewBox="0 0 14 22" {...props}>
+    <path
+      d="M7 .75L.75 11.25 7 15l6.25-3.75L7 .75zM.75 12.5L7 21.25l6.25-8.75L7 16.25.75 12.5z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
   </svg>
 );
