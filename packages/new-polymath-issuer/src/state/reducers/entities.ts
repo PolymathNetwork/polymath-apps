48b410edf61a179f42d9c407bce647ccc06bbbba
Jeremias Diaz <monitz87@gmail.com>
2019-01-07 18:27:11 -0300

    feat(new-issuer): add erc20DividendsModule to entity reducer

--- packages/new-polymath-issuer/src/state/reducers/entities.ts
+++ packages/new-polymath-issuer/src/state/reducers/entities.ts
@@ -6,7 +6,7 @@ import {
   TransactionEntity,
   DividendEntity,
   CheckpointEntity,
-  Entities,
+  Erc20DividendsModuleEntity,
 } from '~/types';
 import { combineReducers } from 'redux';
 
@@ -14,10 +14,14 @@ export interface EntitiesState {
   transactions: EntityState<TransactionEntity>;
   checkpoints: EntityState<CheckpointEntity>;
   dividends: EntityState<DividendEntity>;
+  erc20DividendsModules: EntityState<Erc20DividendsModuleEntity>;
 }
 
 export const reducer = combineReducers<EntitiesState>({
   transactions: createEntityReducer<TransactionEntity>('TRANSACTIONS'),
   checkpoints: createEntityReducer<CheckpointEntity>('CHECKPOINTS'),
   dividends: createEntityReducer<DividendEntity>('DIVIDENDS'),
+  erc20DividendsModules: createEntityReducer<Erc20DividendsModuleEntity>(
+    'ERC20_DIVIDENDS_MODULES'
+  ),
 });
