594ab0fc488cddbbfae7b8efb50e3b530e7ce7ab
Adrien Denat <adrien.denat@gmail.com>
2019-01-07 11:03:10 -0200

    chore(new-ui): icon component accepts system width/height props
    instead of html raw props (for API consistency)

--- packages/new-polymath-ui/src/components/Icon/Icon.tsx
+++ packages/new-polymath-ui/src/components/Icon/Icon.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import styled, { StyledProps } from 'styled-components';
-import { color, ColorProps } from 'styled-system';
+import {
+  color,
+  ColorProps,
+  width,
+  WidthProps,
+  height,
+  HeightProps,
+} from 'styled-system';
 
 export interface IconProps extends StyledProps<any> {
   color: ColorProps;
+  width: WidthProps;
+  height: HeightProps;
   Asset: React.ComponentType<React.SVGAttributes<SVGElement>>;
 }
 
@@ -12,6 +21,8 @@ export const Icon = styled(({ Asset, color, ...props }: IconProps) => (
 ))`
   vertical-align: middle;
   ${color};
+  ${width};
+  ${height};
 `;
 
 // TODO @grsmto: remove when https://github.com/pedronauck/docz/issues/337 is resolved
