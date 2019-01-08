f196c7317c9bba9465a074a3098855512c270c71
Adrien Denat <adrien.denat@gmail.com>
2019-01-08 20:04:47 -0200

    chore(new-ui): refactor TextEllipsis component to use helper func

--- packages/new-polymath-ui/src/components/TextEllipsis/TextEllipsis.tsx
+++ packages/new-polymath-ui/src/components/TextEllipsis/TextEllipsis.tsx
@@ -1,34 +1,12 @@
-import React, { Component } from 'react';
-
-import * as sc from './styles';
+import React, { FC } from 'react';
+import { formatters } from '@polymathnetwork/new-shared';
 
 interface TextEllipsisProps {
-  ellipsedChars: number;
+  size: number;
+  children: string;
 }
 
-export class TextEllipsis extends Component<TextEllipsisProps> {
-  public elRef: React.RefObject<HTMLSpanElement> = React.createRef();
-
-  componentDidMount() {
-    if (!this.elRef.current || !this.elRef.current.textContent) {
-      return;
-    }
-
-    if (this.elRef.current.offsetWidth < this.elRef.current.scrollWidth) {
-      var text = this.elRef.current.textContent;
-      this.elRef.current.dataset.tail = text.slice(
-        text.length - this.props.ellipsedChars
-      );
-    }
-  }
-
-  public render() {
-    const { children, ellipsedChars, ...rest } = this.props;
-
-    return (
-      <sc.Wrapper ref={this.elRef} {...rest}>
-        {children}
-      </sc.Wrapper>
-    );
-  }
-}
+export const TextEllipsis: FC<TextEllipsisProps> = props => {
+  const { children, size, ...rest } = props;
+  return <span {...rest}>{formatters.toShortAddress(children, { size })}</span>;
+};
