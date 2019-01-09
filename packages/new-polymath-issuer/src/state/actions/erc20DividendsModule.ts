c25e5e0b1dba137eb5e69598bcd3b5230e8e0049
Jeremias Diaz <monitz87@gmail.com>
2019-01-09 15:21:44 -0300

    feat(new-issuer): move enableErc20DividendsModuleStart action

--- packages/new-polymath-issuer/src/state/actions/erc20DividendsModule.ts
+++ packages/new-polymath-issuer/src/state/actions/erc20DividendsModule.ts
@@ -1,13 +1,8 @@
 import { Erc20DividendsModuleEntity } from '~/types';
 import { createEntityActions } from '~/state/helpers/createEntityActions';
-import { createStandardAction } from 'typesafe-actions';
 
 const { createAction, updateAction, deleteAction } = createEntityActions<
   Erc20DividendsModuleEntity
 >('ERC20_DIVIDENDS_MODULES');
 
-const enableModule = createStandardAction('ERC20_DIVIDENDS_MODULES/ENABLE')<{
-  securityTokenSymbol: string;
-}>();
-
-export { createAction, updateAction, deleteAction, enableModule };
+export { createAction, updateAction, deleteAction };
