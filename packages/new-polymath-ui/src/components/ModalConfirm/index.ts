e86de78382e14cd0e8cce0f27e398bf31cda0e58
Adrien Denat <adrien.denat@gmail.com>
2019-01-09 13:00:16 -0200

    feat(new-ui): add ModalConfirm and ModalConfirmTx components

---
+++ packages/new-polymath-ui/src/components/ModalConfirm/index.ts
@@ -0,0 +1,8 @@
+import { Modal } from '~/components/Modal';
+
+import { ModalConfirm } from './ModalConfirm';
+
+ModalConfirm.Header = Modal.Header;
+ModalConfirm.Body = Modal.Body;
+
+export default ModalConfirm;
---
+++ packages/new-polymath-ui/src/components/ModalConfirmTx/ModalConfirm.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import { Button } from '~/components/Button';
+import { Modal } from '~/components/Modal';
+
+export interface ModalConfirmProps {
+  children: Node;
+  isActionDisabled: boolean;
+  actionButtonText: string;
+  cancelButtonText: string;
+  onClose: Function;
+  onSubmit: Function;
+  isOpen: boolean;
+}
+
+export const ModalConfirm = (props: ModalConfirmProps) => {
+  const {
+    isOpen,
+    onClose,
+    onSubmit,
+    isActionDisabled,
+    actionButtonText,
+    cancelButtonText,
+    children,
+    ...otherProps
+  } = props;
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={onSubmit}
+      {...otherProps}
+    >
+      {children}
+      <Modal.Footer>
+        <Button className="cancel-btn" kind="secondary" onClick={onClose}>
+          {cancelButtonText}
+        </Button>
+        <Button onClick={onSubmit} disabled={isActionDisabled}>
+          {actionButtonText}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+ModalConfirm.defaultProps = {
+  isActionDisabled: false,
+  actionButtonText: 'Confirm',
+  cancelButtonText: 'Cancel',
+  onClose: null,
+};
