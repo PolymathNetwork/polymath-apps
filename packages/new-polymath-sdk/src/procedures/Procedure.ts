8c177d6f5e68f9ca51230dcf90039aad77b85092
RafaelVidaurre <narzerus@gmail.com>
2019-01-10 16:22:48 -0300

    refactor(sdk): remove unneeded boolean on Procedure

--- packages/new-polymath-sdk/src/procedures/Procedure.ts
+++ packages/new-polymath-sdk/src/procedures/Procedure.ts
@@ -12,7 +12,7 @@ import { PostTransactionResolver } from '~/PostTransactionResolver';
 function isProcedure<T extends any[]>(
   value: any
 ): value is ProcedureType<T[0]> {
-  return value.isProcedure;
+  return value instanceof Procedure;
 }
 
 export interface ProcedureType<Args = any> {
@@ -25,7 +25,6 @@ type MethodOrProcedure<A extends any[]> =
 
 // NOTE @RafaelVidaurre: We could add a preparation state cache to avoid repeated transactions and bad validations
 export abstract class Procedure<Args> {
-  public static readonly isProcedure = true;
   protected args: Args;
   protected context: Context;
   private transactions: TransactionSpec<any>[] = [];
