464bcd80a3d1e44220e15093831178ba82bbd9b8
Adrien Denat <adrien.denat@gmail.com>
2019-01-11 16:30:48 -0200

    chore(new-issuer): improve setup of polymath-ui in issuer

--- packages/new-polymath-ui/src/index.ts
+++ packages/new-polymath-ui/src/index.ts
@@ -1,9 +1,10 @@
 import * as theme from './styles/theme';
 
+// Styles
 export * from './styles';
-export { GlobalStyles } from './styles/GlobalStyles';
-export { ModalSequence } from './components/ModalSequence';
+export { theme };
+
+// Components
+export { ModalTransactionQueue } from './components/ModalTransactionQueue';
 export { Button } from './components/Button';
 export { Loading } from './components/Loading';
-
-export { theme };
