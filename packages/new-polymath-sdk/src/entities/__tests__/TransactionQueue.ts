6295d40e95a4224a99eb8233d5ac2370b4bc6037
RafaelVidaurre <narzerus@gmail.com>
2019-01-15 16:53:35 -0300

    fix(sdk): transaction queue starting as running instead of idle

--- packages/new-polymath-sdk/src/entities/__tests__/TransactionQueue.ts
+++ packages/new-polymath-sdk/src/entities/__tests__/TransactionQueue.ts
@@ -1,3 +1,4 @@
+import { types } from '@polymathnetwork/new-shared';
 import { MockedContract, getMockTransactionSpec } from '~/testUtils';
 import { TransactionQueue } from '../TransactionQueue';
 
@@ -10,17 +11,60 @@ describe('TransactionQueue', () => {
 
   describe('constructor', () => {
     test('initializes properly', () => {
-      const transaction = getMockTransactionSpec(testContract.fakeTxOne, [
-        'someString',
-      ]);
+      const transaction = getMockTransactionSpec(testContract.fakeTxOne, []);
       const transactionQueue = new TransactionQueue([transaction]);
       expect(transactionQueue).toBeInstanceOf(TransactionQueue);
       expect(transactionQueue.run()).toBeInstanceOf(Promise);
     });
   });
 
-  describe('execution', () => {
-    test('resolves when it is finished', async () => {
+  describe('#onTransactionStatusChange', () => {
+    test("calls listener when any transaction's status is updated", async () => {
+      const contract = new MockedContract({ autoResolve: false });
+
+      const tx1 = getMockTransactionSpec(contract.fakeTxOne, []);
+      const tx2 = getMockTransactionSpec(contract.fakeTxTwo, []);
+      const transactionQueue = new TransactionQueue([tx1, tx2]);
+
+      const polyTx1 = transactionQueue.transactions[0];
+      const polyTx2 = transactionQueue.transactions[1];
+
+      const spyListener = jest.fn();
+
+      transactionQueue.onTransactionStatusChange(spyListener);
+      transactionQueue.run();
+
+      contract.fakeTxOnePromiEvent.resolve();
+      await polyTx1.promise;
+      const tx1Matcher = expect.objectContaining({ uid: polyTx1.uid });
+      const tx2Matcher = expect.objectContaining({ uid: polyTx2.uid });
+      const queueMatcher = expect.objectContaining({
+        uid: transactionQueue.uid,
+      });
+
+      expect(spyListener).toHaveBeenLastCalledWith(tx1Matcher, queueMatcher);
+
+      contract.fakeTxTwoPromiEvent.resolve();
+
+      await polyTx2.promise;
+      expect(spyListener).toHaveBeenLastCalledWith(tx2Matcher, queueMatcher);
+    });
+  });
+
+  describe('#toPojo', () => {
+    test('returns a plain object representing the entity', () => {
+      const txOne = getMockTransactionSpec(testContract.fakeTxOne, [
+        'stringOne',
+      ]);
+
+      const transactionQueue = new TransactionQueue([txOne]);
+
+      expect(types.isPojo(transactionQueue.toPojo())).toBeTruthy();
+    });
+  });
+
+  describe('#run', () => {
+    test('runs the queue sequentially and resolves when done', async () => {
       const txOne = getMockTransactionSpec(testContract.fakeTxOne, [
         'stringOne',
       ]);
@@ -28,11 +72,45 @@ describe('TransactionQueue', () => {
         'stringTwo',
       ]);
       const transactionQueue = new TransactionQueue([txOne, txTwo]);
+      const t1Promise = transactionQueue.transactions[0].promise;
+      const t2Promise = transactionQueue.transactions[1].promise;
 
-      await transactionQueue.run();
+      transactionQueue.transactions[0].promise.then(() => {
+        expect(txOne.method).toHaveBeenCalled();
+        expect(txTwo.method).not.toHaveBeenCalled();
+      });
+      transactionQueue.transactions[1].promise.then(() => {
+        expect(txTwo.method).toHaveBeenCalled();
+      });
 
-      expect(testContract.fakeTxOne).toHaveBeenCalled();
-      expect(testContract.fakeTxTwo).toHaveBeenCalled();
+      transactionQueue.run();
+
+      await transactionQueue.promise;
+      await Promise.all([t1Promise, t2Promise]);
+    });
+
+    test("updates the queue's status correctly", () => {
+      const contract = new MockedContract({ autoResolve: false });
+      const txOne = getMockTransactionSpec(contract.fakeTxOne, ['stringOne']);
+      const txTwo = getMockTransactionSpec(contract.fakeTxTwo, ['stringTwo']);
+
+      const transactionQueue = new TransactionQueue([txOne, txTwo]);
+
+      expect(transactionQueue.status).toEqual(
+        types.TransactionQueueStatus.Idle
+      );
+      transactionQueue.run();
+      expect(transactionQueue.status).toEqual(
+        types.TransactionQueueStatus.Running
+      );
+      contract.fakeTxOnePromiEvent.resolve();
+      expect(transactionQueue.status).toEqual(
+        types.TransactionQueueStatus.Running
+      );
+      contract.fakeTxOnePromiEvent.resolve();
+      expect(transactionQueue.status).toEqual(
+        types.TransactionQueueStatus.Running
+      );
     });
   });
 });
