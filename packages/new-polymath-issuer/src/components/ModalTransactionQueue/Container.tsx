dbfe9f71d2cdbfe99935007d476ce1952f066e81
Adrien Denat <adrien.denat@gmail.com>
2019-01-16 19:16:31 -0200

    chore(new-issuer): add basic presenter for dividends

--- packages/new-polymath-issuer/src/components/ModalTransactionQueue/Container.tsx
+++ packages/new-polymath-issuer/src/components/ModalTransactionQueue/Container.tsx
@@ -1,6 +1,12 @@
-import React, { Component, Dispatch } from 'react';
+import React, { Fragment, Component, Dispatch } from 'react';
 import { connect } from 'react-redux';
-import { ModalTransactionQueue } from '@polymathnetwork/new-ui';
+import {
+  ModalTransactionQueue,
+  ModalConfirmTransactionQueue,
+  TransactionItem,
+  Paragraph,
+  icons,
+} from '@polymathnetwork/new-ui';
 import { createGetActiveTransactionQueue } from '~/state/selectors';
 import { RootState } from '~/state/store';
 import { ActionType } from 'typesafe-actions';
@@ -28,24 +34,58 @@ const mapStateToProps = () => {
 };
 
 export class ContainerBase extends Component<Props> {
+  state = {
+    isConfirming: false,
+    isConfirmed: false,
+  };
+
   public onContinue = () => {
     const { dispatch } = this.props;
 
     dispatch(unsetActiveTransactionQueue());
   };
+
+  public handleConfirm = () => {
+    this.setState({
+      isConfirmed: true,
+    });
+  };
+
   public render() {
     const { transactionQueue } = this.props;
+    const { isConfirmed, isConfirming } = this.state;
 
     if (!transactionQueue) {
       return null;
     }
 
     return (
-      <ModalTransactionQueue
-        isOpen={!!transactionQueue}
-        transactionQueue={transactionQueue}
-        onContinue={this.onContinue}
-      />
+      <Fragment>
+        <ModalConfirmTransactionQueue
+          isOpen={isConfirming}
+          onSubmit={this.handleConfirm}
+        >
+          <ModalConfirmTransactionQueue.Header>
+            {transactionQueue.procedureType}
+          </ModalConfirmTransactionQueue.Header>
+          <Paragraph fontSize={2}>{transactionQueue.description}</Paragraph>
+          <div>
+            {transactionQueue.transactions.map(transaction => (
+              <TransactionItem
+                key={transaction.uid}
+                Asset={icons.SvgErc20}
+                title="The transaction will be used to ...."
+                description="2000000 TOKEN"
+              />
+            ))}
+          </div>
+        </ModalConfirmTransactionQueue>
+        <ModalTransactionQueue
+          isOpen={!!transactionQueue && isConfirmed}
+          transactionQueue={transactionQueue}
+          onContinue={this.onContinue}
+        />
+      </Fragment>
     );
   }
 }
--- packages/new-polymath-issuer/src/pages/SecurityTokensDividends/Presenter.tsx
+++ packages/new-polymath-issuer/src/pages/SecurityTokensDividends/Presenter.tsx
@@ -1,3 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Page,
+  Heading,
+  Grid,
+  ButtonLarge,
+  CardFeatureState,
+  icons,
+} from '@polymathnetwork/new-ui';
+import { ModalTransactionQueue } from '~/components';
 
-export const Presenter = () => <div>[SecurityTokensDividends]</div>;
+export class Presenter extends Component {
+  public handleEnableDividend = () => {};
+
+  public render() {
+    return (
+      <Page title="Dividends">
+        <Heading variant="h1" as="h1">
+          Dividends
+        </Heading>
+        <Grid.Row>
+          <Grid.Col gridSpan={[12, 12, 7]}>
+            <Heading variant="h4">
+              Enable the Dividends module to distribute dividends to all token
+              holders. Distribution events can be added, modified, enabled or
+              disabled at any time. To distribute dividends to all your token
+              holders, start by creating a dividend checkpoint. A dividend
+              checkpoint will report the percentage ownership of security tokens
+              per wallet address. This percentage will be used to calculate the
+              amount of dividends owed to each wallet address.
+            </Heading>
+          </Grid.Col>
+          <Grid.Col gridSpan={[12, 12, 5]}>
+            <CardFeatureState status="inactive" IconAsset={icons.SvgDividends}>
+              <Heading color="primary" mt={2}>
+                Ability to distribute Dividends
+              </Heading>
+              <ButtonLarge kind="secondary" onClick={this.handleEnableDividend}>
+                Enable
+              </ButtonLarge>
+            </CardFeatureState>
+          </Grid.Col>
+        </Grid.Row>
+        <ModalTransactionQueue />
+      </Page>
+    );
+  }
+}
