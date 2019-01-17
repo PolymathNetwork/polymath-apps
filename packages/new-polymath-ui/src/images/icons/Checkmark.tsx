887298c2b6a2498c4d3f1e1175bb9b4ce9e072b4
Adrien Denat <adrien.denat@gmail.com>
2019-01-17 18:29:48 -0200

    chore(new-ui): add missing icons

--- packages/new-polymath-ui/src/images/icons/Checkmark.tsx
+++ packages/new-polymath-ui/src/images/icons/Checkmark.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 export const SvgCheckmark = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="1em" height="1em" {...props}>
+  <svg width="1em" height="1em" viewBox="0 0 32 26" {...props}>
     <path
       d="M11.214 17.334L3.829 10.02 0 14.084 11.214 25.19 32 4.602 28.17.81z"
       fill="currentColor"
