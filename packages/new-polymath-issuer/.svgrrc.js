ad746554ca2727213c5f584bae18e474ffa797ac
RafaelVidaurre <narzerus@gmail.com>
2019-01-17 16:09:15 -0300

    fix(new-issuer): tests breaking for file imports

---
+++ packages/new-polymath-issuer/.svgrrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  template(
+    { template },
+    opts,
+    { imports, componentName, props, jsx, exports }
+  ) {
+    const typeScriptTpl = template.smart({ plugins: ['typescript'] });
+    return typeScriptTpl.ast`
+    // This file has been generated automatically by SVGR CLI. Please see README.md for more details.
+    import * as React from 'react';
+    export const ${componentName} = (props: React.SVGProps<SVGSVGElement>) => ${jsx};
+  `;
+  },
+};
