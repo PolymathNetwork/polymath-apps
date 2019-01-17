7551572e66e818e8ba1a6927d395cf9e59d1f782
Adrien Denat <adrien.denat@gmail.com>
2019-01-17 18:29:21 -0200

    chore(new-issuer): reorganise ModalTransactionQueue component

---
+++ packages/new-polymath-ui/src/components/ModalConfirmTransactionQueue/contentMappings.ts
@@ -0,0 +1,71 @@
+import { types } from '@polymathnetwork/new-shared';
+import { SvgErc20 } from '~/images/icons/Erc20';
+
+export const getTransactionText = (transaction: types.TransactionEntity) =>
+  ({
+    [types.PolyTransactionTags.Any]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.Approve]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.GetTokens]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.ReserveSecurityToken]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.CreateSecurityToken]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.CreateCheckpoint]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.CreateErc20DividendDistribution]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.CreateEtherDividendDistribution]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.SetErc20TaxWithholding]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.SetEtherTaxWithholding]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.EnableDividends]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.ReclaimDividendFunds]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+    [types.PolyTransactionTags.WithdrawTaxWithholdings]: {
+      title: 'A transaction title',
+      description: 'A transaction description',
+      Icon: SvgErc20,
+    },
+  }[transaction.tag]);
