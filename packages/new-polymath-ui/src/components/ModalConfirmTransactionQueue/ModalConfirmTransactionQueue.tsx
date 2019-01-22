baf2db63e694e5b49f9719db7834b9d855c137ed
Adrien Denat <adrien.denat@gmail.com>
2019-01-22 19:02:47 -0200

    chore(new-ui): apply right copy to TransactionModal title/desc

--- packages/new-polymath-ui/src/components/ModalConfirmTransactionQueue/ModalConfirmTransactionQueue.tsx
+++ packages/new-polymath-ui/src/components/ModalConfirmTransactionQueue/ModalConfirmTransactionQueue.tsx
@@ -4,7 +4,7 @@ import { ModalConfirm } from '~/components/ModalConfirm';
 import { Paragraph } from '~/components/Paragraph';
 import { TransactionItem } from '~/components/TransactionItem';
 import { ModalStatus } from '~/components/Modal/types';
-import { getTransactionText } from './contentMappings';
+import { getTransactionQueueText, getTransactionText } from './contentMappings';
 
 type ModalConfirmProps = typeHelpers.GetProps<typeof ModalConfirm>;
 
@@ -20,6 +20,7 @@ export const ModalConfirmTransactionQueue: FC<Props> = ({
   transactionQueue,
   ...props
 }) => {
+  const transactionQueueText = getTransactionQueueText(transactionQueue);
   return (
     <ModalConfirm
       isOpen={!!transactionQueue}
@@ -28,19 +29,17 @@ export const ModalConfirmTransactionQueue: FC<Props> = ({
       isCentered={false}
       {...props}
     >
-      <ModalConfirm.Header>
-        {transactionQueue.procedureType}
-      </ModalConfirm.Header>
-      <Paragraph fontSize={2}>{transactionQueue.description}</Paragraph>
+      <ModalConfirm.Header>{transactionQueueText.title}</ModalConfirm.Header>
+      <Paragraph fontSize={2}>{transactionQueueText.description}</Paragraph>
       <div>
         {transactionQueue.transactions.map(transaction => {
-          const { title, description, Icon } = getTransactionText(transaction);
+          const transactionText = getTransactionText(transaction);
           return (
             <TransactionItem
               key={transaction.uid}
-              Asset={Icon}
-              title={title}
-              description={description}
+              Asset={transactionText.Icon}
+              title={transactionText.title}
+              description={transactionText.description}
             />
           );
         })}
