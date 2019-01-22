c3ef549690d1844b1f86bc70a2e1957903076144
Adrien Denat <adrien.denat@gmail.com>
2019-01-22 11:28:09 -0200

    chore(issuer): fix typo on import statement

--- packages/polymath-issuer/src/pages/token/components/MintTokens.js
+++ packages/polymath-issuer/src/pages/token/components/MintTokens.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
-import { Remark, aderaddressShortifier, confirm } from '@polymathnetwork/ui';
+import { Remark, addressShortifier, confirm } from '@polymathnetwork/ui';
 import {
   Icon,
   InlineNotification,
