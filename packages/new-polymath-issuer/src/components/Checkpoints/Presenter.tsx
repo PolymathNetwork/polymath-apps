e4988ac68dbe9d72de36f0a9dd767bf2740e3d2a
Adrien Denat <adrien.denat@gmail.com>
2019-01-23 23:06:52 -0200

    feat(new-ui): add breakpoints list base rendering

--- packages/new-polymath-issuer/src/components/Checkpoints/Presenter.tsx
+++ packages/new-polymath-issuer/src/components/Checkpoints/Presenter.tsx
@@ -1,3 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { types } from '@polymathnetwork/new-shared';
+import { ProgressIndicator } from '@polymathnetwork/new-ui';
+import { Checkpoint } from '~/components/Checkpoint';
 
-export const Presenter = () => <div>[Checkpoints]</div>;
+export interface Props {
+  checkpoints: types.CheckpointEntity[];
+  symbol: string;
+}
+
+export const Presenter = ({ checkpoints }: Props) => (
+  <ProgressIndicator vertical>
+    {checkpoints.map(checkpoint => (
+      <ProgressIndicator.Step
+        label={
+          <Checkpoint
+            key={checkpoint.index}
+            checkpointIndex={checkpoint.index}
+            symbol=""
+          />
+        }
+      />
+    ))}
+  </ProgressIndicator>
+);
--- packages/new-polymath-ui/src/images/icons/Plus.tsx
+++ packages/new-polymath-ui/src/images/icons/Plus.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 export const SvgPlus = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="1em" height="1em" {...props}>
-    <path d="M7 0H5v5H0v2h5v5h2V7h5V5H7z" fillRule="evenodd" />
+  <svg width="1em" height="1em" viewBox="0 0 12 12" {...props}>
+    <path
+      d="M7 0H5v5H0v2h5v5h2V7h5V5H7z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
   </svg>
 );
