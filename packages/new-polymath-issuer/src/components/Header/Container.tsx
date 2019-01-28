16b573bbab5c9b37f530c51fbea2ae85a1432de3
Adrien Denat <adrien.denat@gmail.com>
2019-01-28 14:47:15 -0200

    refactor(new-issuer): use state selector when possible

--- packages/new-polymath-issuer/src/components/Header/Container.tsx
+++ packages/new-polymath-issuer/src/components/Header/Container.tsx
@@ -1,18 +1,25 @@
 import React, { FC } from 'react';
 import { connect } from 'react-redux';
-import { get } from 'lodash';
 import { typeHelpers } from '@polymathnetwork/new-shared';
 import { Header } from '@polymathnetwork/new-ui';
 import { RootState } from '~/state/store';
+import { sessionSelector } from '~/state/selectors';
 
 type HeaderProps = typeHelpers.GetProps<typeof Header>;
 
 export interface Props
   extends Pick<HeaderProps, 'variant' | 'walletAddress' | 'RouterLink'> {}
 
-const mapStateToProps = (state: RootState) => ({
-  walletAddress: get(state, 'session.wallet.address'),
-});
+const mapStateToProps = (state: RootState) => {
+  const { wallet } = sessionSelector(state);
+  let walletAddress;
+
+  if (wallet) {
+    walletAddress = wallet.address;
+  }
+
+  return { walletAddress };
+};
 
 const ContainerBase: FC<Props> & { defaultProps: { variant: 'default' } } = ({
   walletAddress,
