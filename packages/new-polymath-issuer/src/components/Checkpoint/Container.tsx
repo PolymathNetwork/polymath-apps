8e1d8b7c883b011ec6ef41936e71c01898e0d89c
Jeremias Diaz <monitz87@gmail.com>
2019-01-31 18:00:06 -0300

    feat(new-issuer): implement ownership list download logic

--- packages/new-polymath-issuer/src/components/Checkpoint/Container.tsx
+++ packages/new-polymath-issuer/src/components/Checkpoint/Container.tsx
@@ -4,7 +4,9 @@ import { Dispatch } from 'redux';
 import { Presenter } from './Presenter';
 import { DataFetcher } from '~/components/enhancers/DataFetcher';
 import { createCheckpointBySymbolAndIdFetcher } from '~/state/fetchers';
-import { types } from '@polymathnetwork/new-shared';
+import { types, utils } from '@polymathnetwork/new-shared';
+import BigNumber from 'bignumber.js';
+import moment from 'moment';
 
 export interface Props {
   dispatch: Dispatch<any>;
@@ -12,7 +14,50 @@ export interface Props {
   checkpointIndex: number;
 }
 
+interface Row {
+  address: string;
+  balance: string;
+  percentage: number;
+}
+
 export class ContainerBase extends Component<Props> {
+  public downloadOwnershipList = (checkpoint: types.CheckpointEntity) => {
+    const { symbol } = this.props;
+    const { createdAt, investorBalances, totalSupply } = checkpoint;
+    const data: Row[] = investorBalances.map(({ balance, address }) => {
+      const percentage = balance
+        .div(totalSupply)
+        .times(new BigNumber(100))
+        .toNumber();
+
+      return {
+        address,
+        balance: balance.valueOf(),
+        percentage,
+      };
+    });
+
+    const fileName = `checkpoint_${symbol.toUpperCase()}_${moment(
+      createdAt
+    ).format('MM/DD/YYYY')}_${totalSupply}`;
+
+    utils.downloadCsvFile(data, fileName, {
+      fields: [
+        {
+          label: 'Wallet Address',
+          value: 'address',
+        },
+        {
+          label: 'Held Tokens',
+          value: 'balance',
+        },
+        {
+          label: '% of Total Supply',
+          value: 'percentage',
+        },
+      ],
+    });
+  };
   public render() {
     const { symbol, checkpointIndex } = this.props;
     return (
@@ -24,7 +69,9 @@ export class ContainerBase extends Component<Props> {
           }),
         ]}
         render={(data: { checkpoints: types.CheckpointEntity[] }) => {
-          const { checkpoints: [checkpoint] } = data;
+          const {
+            checkpoints: [checkpoint],
+          } = data;
           // TODO @monitz87: pass the actual props to the presenter when it is implemented
           return <Presenter />;
         }}
