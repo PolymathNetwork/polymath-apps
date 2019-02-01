0555e5aac6d6fc1efdc21da6bd86517764f595b4
Adrien Denat <adrien.denat@gmail.com>
2019-02-01 19:16:35 -0200

    feat(new-ui): refactor Tooltip component

--- packages/new-polymath-ui/src/components/Tooltip/styles.ts
+++ packages/new-polymath-ui/src/components/Tooltip/styles.ts
@@ -1,5 +1,89 @@
 import { createGlobalStyle } from '~/styles';
 
+const tooltipSpace = 5;
+const arrowSize = 8;
+
 export const GlobalStyles = createGlobalStyle`
-  .tooltip {}
+  .popper,
+  .tooltip {
+    position: absolute;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
+    text-align: center;
+  }
+
+  .popper .popper__arrow,
+  .tooltip .tooltip-arrow {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    position: absolute;
+    margin: ${arrowSize + tooltipSpace}px;
+  }
+
+  .tooltip .tooltip-arrow,
+  .popper .popper__arrow {
+    border-color: currentColor;
+  }
+
+  .popper[x-placement^="top"],
+  .tooltip[x-placement^="top"] {
+    margin-bottom: ${arrowSize + tooltipSpace}px;
+  }
+
+  .popper[x-placement^="top"] .popper__arrow,
+  .tooltip[x-placement^="top"] .tooltip-arrow {
+    border-width: ${arrowSize}px ${arrowSize}px 0 ${arrowSize}px;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    border-bottom-color: transparent;
+    bottom: ${-arrowSize + 1}px;
+    left: calc(50% - ${arrowSize}px);
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  .popper[x-placement^="bottom"],
+  .tooltip[x-placement^="bottom"] {
+    margin-top: ${arrowSize + tooltipSpace}px;
+  }
+  .tooltip[x-placement^="bottom"] .tooltip-arrow,
+  .popper[x-placement^="bottom"] .popper__arrow {
+    border-width: 0 ${arrowSize}px ${arrowSize}px ${arrowSize}px;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    border-top-color: transparent;
+    top: ${-arrowSize + 1}px;
+    left: calc(50% - ${arrowSize}px);
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  .tooltip[x-placement^="right"],
+  .popper[x-placement^="right"] {
+    margin-left: ${arrowSize + tooltipSpace}px;
+  }
+  .popper[x-placement^="right"] .popper__arrow,
+  .tooltip[x-placement^="right"] .tooltip-arrow {
+    border-width: ${arrowSize}px ${arrowSize}px ${arrowSize}px 0;
+    border-left-color: transparent;
+    border-top-color: transparent;
+    border-bottom-color: transparent;
+    left: ${-arrowSize + 1}px;
+    top: calc(50% - ${arrowSize}px);
+    margin-left: 0;
+    margin-right: 0;
+  }
+  .popper[x-placement^="left"],
+  .tooltip[x-placement^="left"] {
+    margin-right: ${arrowSize + tooltipSpace}px;
+  }
+  .popper[x-placement^="left"] .popper__arrow,
+  .tooltip[x-placement^="left"] .tooltip-arrow {
+    border-width: ${arrowSize}px 0 ${arrowSize}px ${arrowSize}px;
+    border-top-color: transparent;
+    border-right-color: transparent;
+    border-bottom-color: transparent;
+    right: ${-arrowSize + 1}px;
+    top: calc(50% - ${arrowSize}px);
+    margin-left: 0;
+    margin-right: 0;
+  }
 `;
--- packages/new-polymath-ui/src/components/TooltipIcon/TooltipIcon.tsx
+++ packages/new-polymath-ui/src/components/TooltipIcon/TooltipIcon.tsx
@@ -1,21 +1,17 @@
 import React, { Fragment } from 'react';
+import { typeHelpers } from '@polymathnetwork/new-shared';
 import { Tooltip } from '~/components/Tooltip';
 import * as sc from './styles';
-
 import { SvgInfo } from '../../images/icons/Info';
 
-export interface TooltipIconProps extends TooltipProps {
-  description: string;
-}
+type TooltipProps = typeHelpers.GetProps<typeof Tooltip>;
 
-export const TooltipIcon = ({ description, children }: TooltipIconProps) => (
+export interface TooltipIconProps extends TooltipProps {}
+
+export const TooltipIcon = ({ children }: TooltipIconProps) => (
   <Fragment>
-    <sc.Icon
-      aria-describedby={description}
-      Asset={SvgInfo}
-      width="16"
-      height="16"
-    />
-    <Tooltip children={children} />
+    <sc.Icon Asset={SvgInfo} width={16} height={16}>
+      <sc.Tooltip>{children}</sc.Tooltip>
+    </sc.Icon>
   </Fragment>
 );
