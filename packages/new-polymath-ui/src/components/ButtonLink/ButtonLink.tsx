9b1b10415fa34b368a736edf1c368b3a3ae46e80
Adrien Denat <adrien.denat@gmail.com>
2019-02-06 18:49:34 -0200

    fix(new-ui): fix ButtonLink missing typings

--- packages/new-polymath-ui/src/components/ButtonLink/ButtonLink.tsx
+++ packages/new-polymath-ui/src/components/ButtonLink/ButtonLink.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import { typeHelpers } from '@polymathnetwork/new-shared';
 import { Button } from '~/components/Button';
 
-export const ButtonLink = styled(({ RouterLink, ...otherProps }) => (
+type ButtonProps = typeHelpers.GetProps<typeof Button>;
+
+interface Props extends ButtonProps {
+  RouterLink: React.ComponentType<any>;
+}
+
+export const ButtonLink = styled(({ RouterLink, ...otherProps }: Props) => (
   <Button
-    as={props => {
+    as={(props: ButtonProps) => {
       const { variant, iconPosition, ...filteredProps } = props;
       return <RouterLink {...filteredProps} />;
     }}
