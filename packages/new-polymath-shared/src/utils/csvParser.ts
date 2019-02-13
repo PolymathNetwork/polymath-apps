888717136e5033ea24c2c002923b81e2a9142b0f
RafaelVidaurre <narzerus@gmail.com>
2019-02-13 13:55:59 -0300

    refactor(new-shared): remove unnecessary try-catch
    exceptions thrown in promises will automatically reject the promise

--- packages/new-polymath-shared/src/utils/csvParser.ts
+++ packages/new-polymath-shared/src/utils/csvParser.ts
@@ -67,15 +67,15 @@ export interface Column {
  * @param props Object of type Props containing the conversion details
  */
 export const parseCsv = async (props: Props): Promise<ResultProps> => {
-  return new Promise((resolve, reject) => {
+  return new Promise(resolve => {
     interface ResultObject {
       [key: string]: any;
       isRowValid: boolean;
     }
 
     // Prepare the data and errors arrays
-    const result: Array<any> = [];
-    const errors: Array<string> = [];
+    const result: ResultObject[] = [];
+    const errors: string[] = [];
     let totalRows: number = 0;
     let validRows: number = 0;
     let errorRows: number = 0;
@@ -85,159 +85,154 @@ export const parseCsv = async (props: Props): Promise<ResultProps> => {
     let hasExtraRows: boolean = false;
     let hasExtraColumns: boolean = false;
 
-    try {
-      if (!props.header) {
-        for (const headerIndex of Object.keys(props.columns)) {
-          props.columns[parseInt(headerIndex, 10)].index = parseInt(
-            headerIndex,
-            10
-          );
-        }
+    if (!props.header) {
+      for (const headerIndex of Object.keys(props.columns)) {
+        props.columns[parseInt(headerIndex, 10)].index = parseInt(
+          headerIndex,
+          10
+        );
       }
-      const step = (results: ParseResult) => {
-        if (results.data.length === 1) {
-          if (props.header && !headerRead) {
-            // Map the indexes of the header with the file
-            for (const headerIndex of Object.keys(results.data[0])) {
-              const column = _.find(props.columns, col => {
-                return col.name === results.data[0][headerIndex];
-              });
+    }
 
-              if (column) {
-                column.index = parseInt(headerIndex, 10);
-              } else {
-                // The file has a column that is not defined by the columns definition
-                if (props.strict) {
-                  validFile = false;
+    const step = (results: ParseResult) => {
+      if (results.data.length === 1) {
+        if (props.header && !headerRead) {
+          // Map the indexes of the header with the file
+          for (const headerIndex of Object.keys(results.data[0])) {
+            const column = _.find(props.columns, col => {
+              return col.name === results.data[0][headerIndex];
+            });
 
-                  if (
-                    props.errorMessages &&
-                    props.errorMessages.extraColumns &&
-                    !hasExtraColumns
-                  ) {
-                    hasExtraColumns = true;
-                    errors.push(props.errorMessages.extraColumns);
-                  }
+            if (column) {
+              column.index = parseInt(headerIndex, 10);
+            } else {
+              // The file has a column that is not defined by the columns definition
+              if (props.strict) {
+                validFile = false;
+
+                if (
+                  props.errorMessages &&
+                  props.errorMessages.extraColumns &&
+                  !hasExtraColumns
+                ) {
+                  hasExtraColumns = true;
+                  errors.push(props.errorMessages.extraColumns);
                 }
               }
             }
-            headerRead = true;
-
-            if (
-              _.find(props.columns, col => {
-                return col.index === undefined && col.required;
-              })
-            ) {
-              validFile = false;
-
-              if (
-                props.errorMessages &&
-                props.errorMessages.missingRequiredColumns
-              ) {
-                errors.push(props.errorMessages.missingRequiredColumns);
-              }
-            }
-
-            return;
           }
-          totalRows++;
+          headerRead = true;
 
           if (
-            !props.header &&
-            props.columns.length !== results.data[0].length
+            _.find(props.columns, col => {
+              return col.index === undefined && col.required;
+            })
           ) {
             validFile = false;
-          }
-
-          if (
-            props.maxRows !== undefined &&
-            props.maxRows > 0 &&
-            totalRows > props.maxRows
-          ) {
-            ignoredRows++;
 
             if (
               props.errorMessages &&
-              props.errorMessages.rowsExceedMaxLimit &&
-              !hasExtraRows
+              props.errorMessages.missingRequiredColumns
             ) {
-              hasExtraRows = true;
-              errors.push(props.errorMessages.rowsExceedMaxLimit);
+              errors.push(props.errorMessages.missingRequiredColumns);
             }
-
-            return;
           }
 
-          const resultObj: ResultObject = {
-            isRowValid: true,
+          return;
+        }
+        totalRows++;
+
+        if (!props.header && props.columns.length !== results.data[0].length) {
+          validFile = false;
+        }
+
+        if (
+          props.maxRows !== undefined &&
+          props.maxRows > 0 &&
+          totalRows > props.maxRows
+        ) {
+          ignoredRows++;
+
+          if (
+            props.errorMessages &&
+            props.errorMessages.rowsExceedMaxLimit &&
+            !hasExtraRows
+          ) {
+            hasExtraRows = true;
+            errors.push(props.errorMessages.rowsExceedMaxLimit);
+          }
+
+          return;
+        }
+
+        const resultObj: ResultObject = {
+          isRowValid: true,
+        };
+
+        for (const column of props.columns) {
+          // No header, results are passed as array
+          const isValid = _.every(
+            column.validators,
+            (validator: validators.ValidatorFn) =>
+              validator(results.data[0][column.index])
+          );
+          // Papa Parser handles all data types except date, hadle date here
+          let columnValue: any = results.data[0][column.index];
+
+          if (
+            columnValue !== '' &&
+            columnValue !== null &&
+            typeof columnValue !== undefined &&
+            /(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d/.test(
+              columnValue
+            ) &&
+            new Date(columnValue).toString() !== 'Invalid Date'
+          ) {
+            columnValue = new Date(columnValue);
+          }
+
+          resultObj[column.name] = {
+            value: columnValue,
+            isColumnValid: isValid,
           };
 
-          for (const column of props.columns) {
-            // No header, results are passed as array
-            const isValid = _.every(
-              column.validators,
-              (validator: validators.ValidatorFn) =>
-                validator(results.data[0][column.index])
-            );
-            // Papa Parser handles all data types except date, hadle date here
-            let columnValue: any = results.data[0][column.index];
-
-            if (
-              columnValue !== '' &&
-              columnValue !== null &&
-              typeof columnValue !== undefined &&
-              /(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\d\d/.test(
-                columnValue
-              ) &&
-              new Date(columnValue).toString() !== 'Invalid Date'
-            ) {
-              columnValue = new Date(columnValue);
-            }
-
-            resultObj[column.name] = {
-              value: columnValue,
-              isColumnValid: isValid,
-            };
-
-            resultObj.isRowValid = resultObj.isRowValid && isValid;
-          }
-
-          if (typeof props.validateRow === 'function') {
-            resultObj.isRowValid =
-              resultObj.isRowValid && props.validateRow(results.data[0]);
-          }
-
-          result.push(resultObj);
-
-          if (resultObj.isRowValid) {
-            validRows++;
-          } else {
-            errorRows++;
-          }
+          resultObj.isRowValid = resultObj.isRowValid && isValid;
         }
-      };
-      const complete = (results: ParseResult) => {
-        resolve({
-          result,
-          totalRows,
-          validRows,
-          errorRows,
-          ignoredRows,
-          validFile,
-          errors,
-        });
-      };
-      const config = {
-        dynamicTyping: true,
-        skipEmptyLines: true,
-        header: false,
-        step,
-        complete,
-      };
 
-      Papa.parse(props.data, config);
-    } catch (error) {
-      reject(error);
-    }
+        if (typeof props.validateRow === 'function') {
+          resultObj.isRowValid =
+            resultObj.isRowValid && props.validateRow(results.data[0]);
+        }
+
+        result.push(resultObj);
+
+        if (resultObj.isRowValid) {
+          validRows++;
+        } else {
+          errorRows++;
+        }
+      }
+    };
+
+    const complete = (results: ParseResult) => {
+      resolve({
+        result,
+        totalRows,
+        validRows,
+        errorRows,
+        ignoredRows,
+        validFile,
+        errors,
+      });
+    };
+    const config = {
+      dynamicTyping: true,
+      skipEmptyLines: true,
+      header: false,
+      step,
+      complete,
+    };
+
+    Papa.parse(props.data, config);
   });
 };
