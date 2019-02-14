9ca2679b605d61eb5f9ff48a4aac11c951fcfe90
Adrien Denat <adrien.denat@gmail.com>
2019-02-14 19:51:54 -0200

    chore(new-ui): cleanup DOM on each test

--- packages/new-polymath-ui/config/setupTests.ts
+++ packages/new-polymath-ui/config/setupTests.ts
@@ -1,4 +1,9 @@
 import 'babel-polyfill';
+import 'jest-dom/extend-expect';
+import { cleanup } from '~/testUtils/helpers';
 
 // We mock this module since it cannot be parsed and is ignored by jests transformer for some reason
 jest.mock('typeface-overpass', () => ({}));
+
+// Unmounts React trees that were mounted with render.
+afterEach(cleanup);
