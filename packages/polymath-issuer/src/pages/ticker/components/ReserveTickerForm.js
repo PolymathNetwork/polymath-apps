4a42fead260ecc7657695a505bb992cd07e2039a
Bilal Alghazi <balghazi@gmail.com>
2019-02-15 16:35:18 -0500

    fix(issuer): fis special character validation

--- packages/polymath-issuer/src/pages/ticker/components/ReserveTickerForm.js
+++ packages/polymath-issuer/src/pages/ticker/components/ReserveTickerForm.js
@@ -21,11 +21,11 @@ const formSchema = validator.object().shape({
     .isRequired(requiredMessage)
     .max(10, maxMessage)
     .matches(
-      /^[a-z0-9./-]+$/i,
+      /^[a-z0-9.-]+$/i,
       'Only alphanumeric characters, hyphens and periods are allowed.'
     )
     .matches(/^[a-z0-9].*/i, 'Ticker must start with a letter or number.')
-    .matches(/.[a-z0-9]+$/i, 'Ticker must end with a letter or number.')
+    .matches(/.*[a-z0-9]$/i, 'Ticker must end with a letter or number.')
     .matches(
       /^(?!.*\.\.)(?!.*\.\-)(?!.*-\.)(?!.*--).*$/i,
       'Ticker must not contain 2 special characters together.'
