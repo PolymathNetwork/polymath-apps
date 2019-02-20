56af5a99c86bc714c84556df6e44c862f9ccd731
Jeremias Diaz <monitz87@gmail.com>
2019-02-20 17:58:45 -0300

    feat(sdk): add approve function to LowLevel Erc20 and a validity check

--- packages/new-polymath-sdk/src/LowLevel/Erc20.ts
+++ packages/new-polymath-sdk/src/LowLevel/Erc20.ts
@@ -2,11 +2,31 @@ import { TransactionObject } from 'web3/eth/types';
 import { Contract } from './Contract';
 import { Context } from './LowLevel';
 import { ERC20Abi } from './abis/ERC20Abi';
-import { GenericContract } from './types';
+import { GenericContract, ApproveArgs } from './types';
+import BigNumber from 'bignumber.js';
 
 interface Erc20Contract extends GenericContract {
   methods: {
     symbol(): TransactionObject<string>;
+    approve(address: string, amount: BigNumber): TransactionObject<void>;
+    totalSupply(): TransactionObject<number>;
+    decimals(): TransactionObject<number>;
+    balanceOf(address: string): TransactionObject<number>;
+    allowance(owner: string, spender: string): TransactionObject<number>;
+    transfer(address: string, amount: BigNumber): TransactionObject<void>;
+    transferFrom(
+      from: string,
+      to: string,
+      amount: BigNumber
+    ): TransactionObject<void>;
+    decreaseApproval(
+      address: string,
+      amount: BigNumber
+    ): TransactionObject<void>;
+    increaseApproval(
+      address: string,
+      amount: BigNumber
+    ): TransactionObject<void>;
   };
 }
 
@@ -15,7 +35,7 @@ export class Erc20 extends Contract<Erc20Contract> {
     super({ address, abi: ERC20Abi.abi, context });
   }
 
-  public async symbol() {
+  public symbol = async () => {
     let symbol = null;
     try {
       symbol = await this.contract.methods.symbol().call();
@@ -24,5 +44,27 @@ export class Erc20 extends Contract<Erc20Contract> {
     }
 
     return symbol;
-  }
+  };
+
+  public approve = async ({ spender, amount }: ApproveArgs) => {
+    return () =>
+      this.contract.methods
+        .approve(spender, amount)
+        .send({ from: this.context.account });
+  };
+
+  public isValidErc20 = async () => {
+    const { methods } = this.contract;
+
+    try {
+      await methods.symbol().call();
+      await methods.totalSupply().call();
+      await methods.decimals().call();
+      await methods.balanceOf('0x0').call();
+    } catch (_err) {
+      return false;
+    }
+
+    return true;
+  };
 }
