5505076b6a2d4aa2f7a3d9c1636660c86b506a6b
Jeremias Diaz <monitz87@gmail.com>
2019-02-26 17:39:10 -0300

    feat(new-issuer): differentiate between canceled and failed queues
    Also implemented redirection to dividend card after pushing dividend payments

--- packages/new-polymath-issuer/src/state/sagas/procedures/checkpoints.ts
+++ packages/new-polymath-issuer/src/state/sagas/procedures/checkpoints.ts
@@ -5,7 +5,7 @@ import { TransactionQueue } from '@polymathnetwork/sdk';
 import { createCheckpointStart } from '~/state/actions/procedures';
 import { runTransactionQueue } from '~/state/sagas/transactionQueues';
 import { invalidateRequest } from '~/state/actions/dataRequests';
-import { RequestKeys, TransactionQueueResult } from '~/types';
+import { RequestKeys, TransactionQueueResult, QueueStatus } from '~/types';
 
 export function* createCheckpoint(
   action: ActionType<typeof createCheckpointStart>
@@ -19,13 +19,13 @@ export function* createCheckpoint(
   );
 
   try {
-    const { success }: TransactionQueueResult = yield call(
+    const { queueStatus }: TransactionQueueResult = yield call(
       runTransactionQueue,
       transactionQueueToRun
     );
 
     // Queue was canceled or failed
-    if (!success) {
+    if (queueStatus !== QueueStatus.Succeeded) {
       return;
     }
 
