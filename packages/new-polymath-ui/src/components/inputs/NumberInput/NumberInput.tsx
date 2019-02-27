dca45e665c7d80f03f043db8ff36bb13d0c44f79
RafaelVidaurre <narzerus@gmail.com>
2019-02-27 13:09:28 -0300

    test(new-ui): formik proxy

--- packages/new-polymath-ui/src/components/inputs/NumberInput/NumberInput.tsx
+++ packages/new-polymath-ui/src/components/inputs/NumberInput/NumberInput.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, FC } from 'react';
 import BigNumber from 'bignumber.js';
 import { constants } from '@polymathnetwork/new-shared';
 
-import { formikProxy } from '../formikProxy';
+import { FormikProxy, ExternalProps } from '../FormikProxy';
 import { BaseInput } from '../BaseInput';
 
 type Value = number | BigNumber | null;
@@ -260,7 +260,20 @@ export class NumberInputPrimitive extends Component<Props, State> {
   }
 }
 
-const EnhancedNumberInput = formikProxy(NumberInputPrimitive);
+const EnhancedNumberInput: FC<ExternalProps> = ({ field, form }) => (
+  <FormikProxy<Value>
+    field={field}
+    form={form}
+    render={({ onChange, onBlur, name, value }) => (
+      <NumberInputPrimitive
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+        name={name}
+      />
+    )}
+  />
+);
 
 export const NumberInput = Object.assign(EnhancedNumberInput, {
   defaultProps: NumberInputPrimitive.defaultProps,
