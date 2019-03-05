eb4bb005369a3e86a8ad0a8e94e7784328802d96
RafaelVidaurre <narzerus@gmail.com>
2019-03-05 16:14:37 -0300

    fix: fix test issues due to an unmocked css module

--- packages/new-polymath-ui/src/testUtils/setupTests.ts
+++ packages/new-polymath-ui/src/testUtils/setupTests.ts
@@ -5,6 +5,7 @@ import { cleanup } from '~/testUtils/helpers';
 
 // We mock this module since it cannot be parsed and is ignored by jests transformer for some reason
 jest.mock('typeface-overpass', () => ({}));
+jest.mock('simplebar/dist/simplebar.css', () => ({}));
 
 // Unmounts React trees that were mounted with render.
 afterEach(cleanup);
