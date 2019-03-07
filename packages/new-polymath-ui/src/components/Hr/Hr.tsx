61e3f1ff9354ccf1876c3895890226a4e4b05739
Adrien Denat <adrien.denat@gmail.com>
2019-03-07 12:14:32 +0000

    chore(new-ui): style the new Hr component

--- packages/new-polymath-ui/src/components/Hr/Hr.tsx
+++ packages/new-polymath-ui/src/components/Hr/Hr.tsx
@@ -9,5 +9,14 @@ export interface HrProps {
 // export type HrProps = StyledProps<any> & WidthProps & HeightProps & SpaceProps;
 
 export const Hr = styled.hr<HrProps>`
+  width: 100%;
   ${colorProp};
+  border: none;
+  background: none;
+  border-top: 1px solid currentColor;
+  margin: ${({ theme }) => `calc(${theme.space.gridGap} / 2) auto`};
 `;
+
+Hr.defaultProps = {
+  color: 'gray.1',
+};
