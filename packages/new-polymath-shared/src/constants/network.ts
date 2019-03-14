348cae8610554f28336d886cb90aae28d8c9bfc8
Bilal Alghazi <balghazi@gmail.com>
2019-03-14 17:08:49 -0400

    chore(new-shared): add token addresses for mainnet

--- packages/new-polymath-shared/src/constants/network.ts
+++ packages/new-polymath-shared/src/constants/network.ts
@@ -34,6 +34,10 @@ export const TokenAddresses = {
   [NetworkIds.Mainnet]: {
     [types.Tokens.Dai]: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
     [types.Tokens.Poly]: '0x9992ec3cf6a55b00978cddf2b27bc6882d88d1ec',
+    [types.Tokens.Gusd]: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd',
+    [types.Tokens.Pax]: '0x8e870d67f660d95d5be530380d0ec0bd388289e1',
+    [types.Tokens.Usdc]: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    [types.Tokens.Usdt]: '0xdac17f958d2ee523a2206206994597c13d831ec7',
   },
   [NetworkIds.Ropsten]: {
     [types.Tokens.Dai]: '0x0',
