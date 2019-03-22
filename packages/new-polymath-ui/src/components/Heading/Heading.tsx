821dc44acb4bffe2c9b8e03f6d1974fc481523ea
Shannon Clarke <shannonajclarke@gmail.com>
2019-03-22 04:00:33 -0400

    chore(new-ui): add word wrap to dividend card heading

--- packages/new-polymath-ui/src/components/Heading/Heading.tsx
+++ packages/new-polymath-ui/src/components/Heading/Heading.tsx
@@ -31,6 +31,8 @@ const StyledHeading = styled(Box)<HeadingProps>`
   ${fontWeight};
   ${lineHeight};
   ${fontSize};
+  width: 100%;
+  word-wrap: break-word;
 `;
 
 // TODO @grsmto: remove when https://github.com/pedronauck/docz/issues/337 is resolved
