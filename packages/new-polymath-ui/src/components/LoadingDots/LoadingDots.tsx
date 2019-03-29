e7bb1f997ebd4ede350eeba4cd9f8d54234a9686
Bilal Alghazi <balghazi@gmail.com>
2019-03-29 09:22:50 -0400

    feat(new-ui): loading dots component

---
+++ packages/new-polymath-ui/src/components/LoadingDots/LoadingDots.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as sc from './styles';
+
+export const LoadingDots = () => {
+  return (
+    <sc.Wrapper>
+      <span />
+      <span />
+      <span />
+    </sc.Wrapper>
+  );
+};
