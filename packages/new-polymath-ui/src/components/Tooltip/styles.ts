cbd2148a86169901b52fe060b0144af5c4c59178
Adrien Denat <adrien.denat@gmail.com>
2019-03-29 13:29:06 +0000

    chore(new-ui): component Tooltip now defines explicit z-index

--- packages/new-polymath-ui/src/components/Tooltip/styles.ts
+++ packages/new-polymath-ui/src/components/Tooltip/styles.ts
@@ -9,6 +9,7 @@ export const GlobalStyles = createGlobalStyle`
     position: absolute;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1);
     text-align: center;
+    z-index: ${({ theme }) => theme.zIndexes.tooltips};
   }
 
   .popper .popper__arrow,
