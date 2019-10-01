9f782efcd0fc4f2b2a8204efc844e149b70314fb
Fahd <fahd@polymath.network>
2019-10-01 12:13:50 -0400

    chore(issuer): add unit tests

---
+++ packages/polymath-issuer/src/pages/restrictions/__tests__/RestrictionDetails.js
@@ -0,0 +1,37 @@
+import RestrictionDetails from '../components/RestrictionDetails';
+import renderer from 'react-test-renderer';
+import React from 'react';
+import configureStore from 'redux-mock-store';
+import { theme, GlobalStyles } from '@polymathnetwork/ui';
+import { shallow, mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
+import moment from 'moment';
+import { Provider } from 'react-redux';
+const mockStore = configureStore([]);
+
+describe('Restriction Details Component', () => {
+  it('should render with the details', () => {
+    const props = {
+      restriction: {
+        startTime: moment.unix(1),
+        endTime: moment(1),
+        allowedTokens: 500,
+        restrictionType: 0,
+      },
+    };
+    const tree = shallow(<RestrictionDetails {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('should render a percentage when restrictionType is 1', () => {
+    const props = {
+      restriction: {
+        startTime: moment.unix(1),
+        endTime: moment(1),
+        allowedTokens: 0.1,
+        restrictionType: 1,
+      },
+    };
+    const tree = shallow(<RestrictionDetails {...props} />);
+    expect(tree).toMatchSnapshot();
+  });
+});
