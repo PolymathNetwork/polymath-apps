f0cf8434f8359d26dd0da5efba6dad17a2f0117f
Fahd <fahd@polymath.network>
2020-01-14 14:18:12 -0500

    add more unit tests

--- packages/polymath-issuer/src/pages/compliance/__tests__/PartialTransferTable.js
+++ packages/polymath-issuer/src/pages/compliance/__tests__/PartialTransferTable.js
@@ -22,4 +22,39 @@ describe('Partial Transfer Modal', () => {
     const tree = shallow(<PartialTransferTable {...props} />);
     expect(tree).toMatchSnapshot();
   });
+
+  it('should set state to open when handleOpen is triggered', () => {
+    const props = {
+      fetchWhitelist: jest.fn(),
+      fetchManagers: jest.fn(),
+      fetchPartialTransfers: jest.fn(),
+      theme,
+    };
+    const tree = shallow(<PartialTransferTable {...props} />);
+    tree.instance().handleOpen();
+    expect(tree.state().isPartialTransferModalOpen).toBe(true);
+  });
+
+  it('should set state to closed when handleClose is triggered', () => {
+    const props = {
+      fetchWhitelist: jest.fn(),
+      fetchManagers: jest.fn(),
+      fetchPartialTransfers: jest.fn(),
+      theme,
+    };
+    const tree = shallow(<PartialTransferTable {...props} />);
+    tree.instance().handleClose();
+    expect(tree.state().isPartialTransferModalOpen).toBe(false);
+  });
+
+  it('should call function to delete the address(id)', () => {
+    const id = '0x121212';
+    const props = {
+      removeAddressFromPartialExempt: jest.fn(),
+      theme,
+    };
+    const tree = shallow(<PartialTransferTable {...props} />);
+    tree.instance().handleDelete(id);
+    expect(props.removeAddressFromPartialExempt).toHaveBeenCalledWith(id);
+  });
 });
